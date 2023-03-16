e6d7fa0a8991d9a2d0c439bd0e1e8eea21ac31d8
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-03-16 19:36:51 +0400

    use stack for branching

--- aword.c
+++ aword.c
@@ -27,14 +27,14 @@ void gut_rotate(long);
 N(trace) { gut_line_to((ρ + 1) * δ, δ), ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ); }
 N(rotate) { gut_rotate(δ), ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ); }
 #define T(aw) {.q = 0}, {{aw}}, {.t = 0}, {.q = 0}, {{trace}}, {.t = 0},
-#define D_(...) {.t = (τext[]){T(p_return) __VA_ARGS__}},
-#define DB(...) {.t = (τext[]){T(m_return) T(o)}}, {{bark}}, D_
-#define B(...) {.t = (τext[]){T(m_return) T(o)}}, {{bark}}, {.t = (τext[]){T(p_return) __VA_ARGS__}},
-N(p_return) { ω[1].t[3].c(α, ω[1].t + 3, ο, ρ, 1); }
-N(m_return) { ω[1].t[-3].c(α, ω[1].t - 3, ο, ρ, -1); }
+#define D_(...) {.t = (τext[]){T(nop) __VA_ARGS__}},
+#define DB(...) {.t = (τext[]){T(nop) __VA_ARGS__}}, {{bark}}, D_
+#define B(...) DB(T(o))(__VA_ARGS__)
+N(nop) {}
 N(bark) {
   τext *β = ω[δ].t + 1;
-  β[1].t = ω, β[3].c(α, β + 3, ο, ρ, 1);
+  β[3].c(α, β + 3, ο, ρ, 1);
+  ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ);
 }
 
 void init_gut();
@@ -43,11 +43,14 @@ int main(int argc, char **argv) {
   init_gut();
   // clang-format off
   τext ω[] = {
-               T(b)
-               T(m)
-        DB(T(m)T(o))(T(m)T(o))
-               T(m)
-               T(o)
+                        T(b)
+                        T(m)
+            DB(T(m)T(m)T(o))(T(m)B(T(m)
+                                   T(m)
+                                   T(m)
+                                   T(o))T(o))
+                        T(m)
+                        T(o)
              };
   // clang-format on
   τext ο[512];
