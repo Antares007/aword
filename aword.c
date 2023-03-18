bc32b5a5dbf1030f5fc67f26f022115221a96b03
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-03-18 04:46:40 +0400

    intoduce X macro

--- aword.c
+++ aword.c
@@ -1,5 +1,11 @@
 #define N(args)                                                                \
   void args(long α, struct τext *ω, struct τext *ο, long ρ, long δ)
+#define T(aw) {.q = 0}, {{aw}}, {.t = 0}, {.q = 0}, {{t}}, {.t = 0},
+#define B(...) DB()(__VA_ARGS__)
+#define D(...) DB(__VA_ARGS__)()
+#define D_(...) {.t = (τext[]){T(rtrn) __VA_ARGS__ T(o)}},
+#define DB(...) {.t = (τext[]){T(rtrn) __VA_ARGS__ T(o)}}, {{bark}}, D_
+#define X ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ)
 typedef struct τext {
   union {
     void *v;
@@ -9,11 +15,6 @@ typedef struct τext {
     N((*c));
   };
 } τext;
-#define T(aw) {.q = 0}, {{aw}}, {.t = 0}, {.q = 0}, {{t}}, {.t = 0},
-#define D_(...) {.t = (τext[]){T(rtrn) __VA_ARGS__ T(o)}},
-#define DB(...) {.t = (τext[]){T(rtrn) __VA_ARGS__ T(o)}}, {{bark}}, D_
-#define B(...) DB()(__VA_ARGS__)
-#define D(...) DB(__VA_ARGS__)()
 N(rtrn) {}
 void gut_rotate(long δ);
 N(bark) {
@@ -21,32 +22,46 @@ N(bark) {
   gut_rotate(-δ);
   β[3].c(α, β + 3, ο, ρ, 1);
   gut_rotate(δ);
-  ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ);
+  X;
 }
-N(o) { ω[-3].c(α, ω - 3, ο, ρ, -1); }
-N(b) { ω[3].c(α, ω + 3, ο, (ρ + 1) % 4, 1); }
+N(o) { δ = -1, X; }
+N(b) { ρ = (ρ + 1) % 4, δ = 1, X; }
 void gut_line_to(long, long);
-N(t) { gut_line_to(ρ, δ), ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ); }
-N(m) { ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ); }
+N(t) { gut_line_to(ρ, δ), X; }
 N(p) {
-  if (δ > 0) ω[3 * δ].c(α, ω + 3 * δ, ο, ρ, δ);
-  else if (ρ < 3) ω[3].c(α, ω + 3, ο, ρ + 1, 1);
-  else ω[-3].c(α, ω - 3, ο, ω[-1].q++ % 4, -1);
+  if (δ > 0)
+    X;
+  else if (ρ < 3)
+    ρ++, δ = 1, X;
+  else
+    ρ = ω[-1].q++ % 4, X;
 }
 void init_gut(long fps);
 void clear_gut();
 int main(int argc, char **argv) {
   // clang-format off
-  τext ω[] = {
+  τext*ω = 1 + (τext[]) {
                                                         T(b)
-                                                        T(m)
+                                 DB(T(t)T(t)T(p)T(t)T(t)T(p)T(t)T(t)T(p)T(t)T(t))
+                                   (T(t)T(t)T(p)T(t)T(t)T(p)T(t)T(t)T(p)T(t)T(t))
+                                                        T(t)
+                                                        T(t)
+                                                        T(t)
+                                                        T(t)
+                                                        T(p)
+                                                        T(t)
+                                                        T(t)
+                                                        T(p)
+                                                        T(t)
+                                                        T(t)
+                                                        T(p)
+                                                        T(t)
+                                                        T(t)
                                                         T(o)
-             };
+  };
   // clang-format on
   τext ο[512];
-  long α = 0;
-  init_gut(20);
-  ω[4].c(α, ω + 4, ο, 0, 1);
-  clear_gut();
+  long α = 0, ρ = 0, δ = 1;
+  init_gut(0), X, clear_gut();
   return 0;
 }
