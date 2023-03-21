34336e42cfbc40cd95aabcd973bb3b5786818882
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-03-21 12:49:45 +0400

    nice show

--- aword.c
+++ aword.c
@@ -19,6 +19,34 @@ typedef struct text_t {
     N((*c));
   };
 } text_t;
+#define D_(...) ((text_t[]){T(ret) __VA_ARGS__ T(o)} + 1),
+#define DB(...) ((text_t[]){T(ret) __VA_ARGS__ T(o)} + 1), 0, D_
+void gut_rotate(float theta);
+N(ret) { δ = ο[σ++].q, ω = ο[σ++].t, gut_rotate(-ο[σ++].f), X; }
+#define AWord(name, ...)                                                       \
+  N(name);                                                                     \
+  text_t *name##_texts[] = {__VA_ARGS__};                                      \
+  N(name) {                                                                    \
+    long hands = sizeof(name##_texts) / sizeof(void *) / 3;                    \
+    long hand_index = ω[1].q;                                                  \
+    text_t *hand = name##_texts[1 + hand_index * 3 + δ];                       \
+    ο[--σ].f = δ * (hand_index + 1) * 3.1416 / (hands + 1);                    \
+    gut_rotate(ο[σ].f);                                                        \
+    if (White(==)) {                                                           \
+      ο[--σ].t = ω;                                                            \
+      ο[--σ].q = 1;                                                            \
+    } else if (hand_index + 1 < hands) {                                       \
+      ο[--σ].t = ω - 3;                                                        \
+      ο[--σ].q = 1;                                                            \
+      ω[1].q++;                                                                \
+    } else {                                                                   \
+      ο[--σ].t = ω;                                                            \
+      ο[--σ].q = -1;                                                           \
+      ω[1].q = 0;                                                              \
+    }                                                                          \
+    White(=);                                                                  \
+    ω = hand, X;                                                               \
+  }
 int gut_close_requested();
 void gut_line_to(long, long, const char *);
 N(t) {
@@ -60,93 +88,56 @@ N(ara) {
   }
 AWgoto(Blue) AWgoto(Red);
 N(id) { X; }
-void gut_rotate(float theta);
-#define D_(...) ((text_t[]){T(goto_home) __VA_ARGS__ T(o)} + 1),
-#define DB(...) ((text_t[]){T(goto_home) __VA_ARGS__ T(o)} + 1), 0, D_
-N(goto_home) { δ = ο[σ++].q, ω = ο[σ++].t, gut_rotate(-ο[σ++].f), X; }
-#define AWord(name, ...)                                                       \
-  N(name);                                                                     \
-  text_t *name##_texts[] = {__VA_ARGS__};                                      \
-  N(name) {                                                                    \
-    long hands = sizeof(name##_texts) / sizeof(void *) / 3;                    \
-    long hand_index = ω[1].q;                                                  \
-    text_t *hand = name##_texts[1 + hand_index * 3 + δ];                       \
-    ο[--σ].f = δ * (hand_index + 1) * 3.1416 / (hands + 1);                    \
-    gut_rotate(ο[σ].f);                                                        \
-    if (White(==)) {                                                           \
-      ο[--σ].t = ω;                                                            \
-      ο[--σ].q = 1;                                                            \
-    } else if (hand_index + 1 < hands) {                                       \
-      ο[--σ].t = ω - 3;                                                        \
-      ο[--σ].q = 1;                                                            \
-      ω[1].q++;                                                                \
-    } else {                                                                   \
-      ο[--σ].t = ω;                                                            \
-      ο[--σ].q = -1;                                                           \
-      ω[1].q = 0;                                                              \
-    }                                                                          \
-    White(=);                                                                  \
-    ω = hand, X;                                                               \
-  }
-/*
-N(p) {
-  if (δ > 0)
-    X;
-  else if (ρ < 3)
-    ρ++, δ = 1, X;
-  else
-    ρ = ω[-1].q++ % 4, X;
-}
-*/
-N(B1) { X; }
-N(A1) { X; }
-N(B2) { X; }
-N(A2) { X; }
-N(B3) { X; }
-N(A3) { X; }
-N(B4) { X; }
-N(A4) { X; }
-N(B5) { X; }
-N(A5) { X; }
-N(B6) { X; }
-N(A6) { X; }
-N(B7) { X; }
-N(A7) { X; }
-AWord(                                                   //
-    eval3,                                               //
-    DB(T(A1) T(A1) T(A1) T(A1))(T(B1) T(B1) T(B1) T(B1)) //
-    DB(T(A2) T(A2) T(A2) T(A2))(T(B2) T(B2) T(B2) T(B2)) //
-    DB(T(A3) T(A3) T(A3) T(A3))(T(B3) T(B3) T(B3) T(B3)) //
+N(deda) { X; }
+N(mama) { X; }
+N(gemriel) { X; }
+N(shvils) { X; }
+N(namckhvars) { X; }
+N(sakhls) { X; }
+N(ackhobs) { X; }
+N(ushenebs) { X; }
+AWord(                                      //
+    b3,                                     //
+    DB(T(mama)T(deda)T(shvils))             //
+      (T(shvils)      T(deda)    T(mama)  ) //
+    DB(T(mama)      T(deda)      T(shvils)) //
+      (T(shvils)      T(deda)    T(mama)  ) //
+    DB(T(mama)      T(deda)      T(shvils)) //
+      (T(shvils)      T(deda)    T(mama)  ) //
 );
-AWord(                                                   //
-    eval7,                                               //
-    DB(T(A1) T(A1) T(A1) T(A1))(T(B1) T(B1) T(B1) T(B1)) //
-    DB(T(A2) T(A2) T(A2) T(A2))(T(B2) T(B2) T(B2) T(B2)) //
-    DB(T(A3) T(A3) T(A3) T(A3))(T(B3) T(B3) T(B3) T(B3)) //
-    DB(T(A4) T(A4) T(A4) T(A4))(T(B4) T(B4) T(B4) T(B4)) //
-    DB(T(A5) T(A5) T(A5) T(A5))(T(B5) T(B5) T(B5) T(B5)) //
-    DB(T(A6) T(A6) T(A6) T(A6))(T(B6) T(B6) T(B6) T(B6)) //
-    DB(T(A7) T(A7) T(A7) T(A7))(T(B7) T(B7) T(B7) T(B7)) //
-);                                                       //
-AWord(                                                   //
-    eval1,                                               //
-    DB(T(A1) T(A1) T(A1) T(A1))(T(B1) T(B1) T(B1) T(B1)) //
-);                                                       //
-
-N(fetch) { X; }
-N(print) { X; }
-N(r45) { gut_rotate(3.1416f / 8.f), X; }
+AWord(                                                                   
+    borjgal1,                                                            
+    DB(T(mama) T(mama) T(mama) T(mama))(T(deda) T(deda) T(deda) T(deda)) 
+);                                                                       
 void gut_init(long fps);
 void gut_clear();
+AWord(borjgal,
+  DB(T(mama)T(shvils)T(sakhls)T(ushenebs)T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(b3))
+    (T(mama)T(shvils)T(sakhls)T(ushenebs)T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(b3))
+  DB(T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(b3))
+    (T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(mama) T(shvils) T(sakhls) T(ushenebs)T(b3))
+  DB(T(mama) T(shvils) T(sakhls) T(ushenebs)T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(b3))
+    (T(deda)T(gemriel)T(namckhvars)T(ackhobs)T(mama) T(shvils) T(sakhls) T(ushenebs)T(b3))
+)
 int main(int argc, char **argv) {
   // clang-format off
-  text_t*ω = 1 + (text_t[]) { T(b)
-
-    T(id)T(id)T(id)T(id)
-    T(eval7)T(id)T(id)T(id)T(id)
-    T(eval1)T(id)T(id)T(id)T(id)
-    T(eval3)T(id)T(id)T(id)T(id)
-    T(o) };
+  text_t*ω = 1 + (text_t[]) {
+    T(b)
+    T(mama)   T(shvils) T(sakhls)     T(ushenebs)
+    T(deda)   T(gemriel)T(namckhvars) T(ackhobs)
+    T(borjgal)
+    T(deda)   T(gemriel)T(namckhvars) T(ackhobs)
+    T(mama)   T(shvils) T(sakhls)     T(ushenebs)
+    T(mama)   T(shvils)
+    T(borjgal1)
+    T(sakhls) T(ushenebs)
+    T(deda)   T(gemriel)T(namckhvars) T(ackhobs)
+    T(mama)   T(shvils) T(sakhls)     T(ushenebs)
+    T(borjgal)
+    T(deda)   T(gemriel)T(namckhvars) T(ackhobs)
+    T(mama)   T(shvils) T(sakhls)     T(ushenebs)
+    T(o)
+  };
   // clang-format on
   text_t ο[128 * 4];
   long τ = 0, α = 128, β = 256, ρ = 3, δ = 1, σ = 384;
--- gut.c
+++ gut.c
@@ -2,13 +2,13 @@
 #include <raylib.h>
 #include <stdint.h>
 #include <unistd.h>
-#define SW 400
-#define SH 400
-#define PW 3
-#define PH 3
+#define SW 800
+#define SH 700
+#define PW 1
+#define PH 1
 #define BGColor BLACK
 
-static float base[] = {SW / 2.f, 2.f}, point[] = {SW / 2.f, 2.f},
+static float base[] = {SW / 2.f, 30.f}, point[] = {SW / 2.f, 30.f},
              direction[] = {0, 1};
 int gut_close_requested() { return WindowShouldClose(); }
 // only 90⁰ rotations δ = -1 | 1
@@ -57,7 +57,7 @@ void gut_clear() { CloseWindow(); }
 static uint32_t screen[SW * SH] = {};
 static void clear_screen() {
   for (long i = 0; i < SW * SH; i++)
-    screen[i] = *(uint32_t *)&BGColor;
+    screen[i] = 0;
 }
 static void render_screen() {
   for (long i = 0; i < SW * SH; i++)
@@ -65,7 +65,7 @@ static void render_screen() {
       DrawRectangle((i % SW) * PW, i / SW * PH, PW, PH, *(Color *)&screen[i]);
   for (long i = 0; i < texts_length; i++)
     DrawText(texts_content[i], texts_positions[i][0] * PW,
-             texts_positions[i][1] * PH, 25, *(Color *)&texts_colors[i]);
+             texts_positions[i][1] * PH, 5, *(Color *)&texts_colors[i]);
 }
 static void draw_frame() {
   BeginDrawing();
