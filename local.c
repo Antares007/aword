a76e526d2efc4505ebdb97fd235a5d6fc430ec58
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-03 07:05:26 +0400

    add font

--- local.c
+++ local.c
@@ -31,10 +31,13 @@ enum { AWords };
 static Color ray_colors[] = {
     YELLOW, RED, GREEN, BLUE, BLACK, DARKBLUE, LIME, MAROON, GOLD,
 };
+static Font font;
 void draw_aword(const char *text, char exits[8][2]) {
   const int vspase = 10;
-  const int width = 20;
-  DrawRectangle(-width, -vspase * 6, width * 2, vspase * 11,
+  const int width = 15;
+  Vector2 spos = {-width, -vspase * 6};
+  Vector2 dim = {width * 2, vspase * 11};
+  DrawRectangle(spos.x, spos.y, dim.x, dim.y,
                 (Color){.r = 0x87, .g = 0xce, .b = 0xeb, .a = 0xff});
   static char entrances[8][2] = {{3, -1}, {2, -1}, {1, -1}, {0, -1},
                                  {0, 1},  {1, 1},  {2, 1},  {3, 1}};
@@ -49,25 +52,26 @@ void draw_aword(const char *text, char exits[8][2]) {
     Vector2 startControlPos = {-(width / 14.f) * sr, sr * vspase};
     Vector2 endPos = {-width * ei, er * vspase};
     Vector2 endControlPos = {(width / 14.f) * er, er * vspase};
-    float thick = 2;
+    float thick = vspase / 5.f;
     Color color = ray_colors[sr + 4];
     DrawLineBezierCubic(startPos, endPos, startControlPos, endControlPos, thick,
                         color);
     if (si == 1)
-      DrawTriangle((Vector2){startPos.x - vspase / 2.f, startPos.y},
-                   (Vector2){startPos.x, startPos.y + 5},
-                   (Vector2){startPos.x, startPos.y - 5}, color);
+      DrawTriangle((Vector2){startPos.x - vspase, startPos.y},
+                   (Vector2){startPos.x, startPos.y + vspase / 2.f},
+                   (Vector2){startPos.x, startPos.y - vspase / 2.f}, color);
     else
-      DrawTriangle((Vector2){startPos.x, startPos.y - 5},
-                   (Vector2){startPos.x, startPos.y + 5},
-                   (Vector2){startPos.x + vspase / 2.f, startPos.y}, color);
+      DrawTriangle((Vector2){startPos.x, startPos.y - vspase / 2.f},
+                   (Vector2){startPos.x, startPos.y + vspase / 2.f},
+                   (Vector2){startPos.x + vspase, startPos.y}, color);
   }
-  DrawText(text, -width + 5, -vspase * 6, 10, BLACK);
+  DrawTextEx(font, text, spos, width * .8f, 0, BLACK);
 }
 int main(void) {
-  InitWindow(1500, 600, "AWord Drawing");
+  InitWindow(1850, 600, "AWord Drawing");
   SetTargetFPS(0);
-  float zoom = 2.0f;
+  font = LoadFont("NovaMono-Regular.ttf");
+  float zoom = 4.0f;
   while (!WindowShouldClose()) {
     int wheelMove = GetMouseWheelMove();
     if (wheelMove > 0)
@@ -85,7 +89,7 @@ int main(void) {
       camera.rotation = 0.0f;
       camera.zoom = zoom;
       camera.offset =
-          (Vector2){100 + i * 41 * camera.zoom, GetScreenHeight() / 2.f};
+          (Vector2){100 + i * 31 * camera.zoom, GetScreenHeight() / 2.f};
       BeginMode2D(camera);
       draw_aword(aword_names[awi[i]], aword_rays[awi[i]]);
     }
