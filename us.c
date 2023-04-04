039f88062900c80245402618be6fe5b11df50b10
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-04 18:12:49 +0400

    sync

--- us.c
+++ us.c
@@ -27,34 +27,30 @@
 // }
 #include <stdio.h>
 #include <unistd.h>
+#define A(...)
 N(print) {
   if (ρ == 3 && δ == 1)
     printf("%s", ο[ω + AW].cs), usleep(20000);
   m(α, ω, ο, ρ, δ, σ);
 }
+N(ws);
+  //  S -> a_f.Fe.EAb_f.Fe.EB
+  //                            .
 N(sword2) {
-  D(B(T(b) Ta(print, "(") T(o)), //
-    B(T(b) Ta(print, ")") T(o)));
-  D(B(T(b) Ta(print, "[") T(o)), //
-    B(T(b) Ta(print, "]") T(o)));
+  D(B(T(b) Ta(print, "f") T(o)), B(T(b) Ta(print, "F") T(o)));
+  D(B(T(b) Ta(print, "e") T(o)), B(T(b) Ta(print, "E") T(o))); //
   toti(α, ω, ο, ρ, δ, σ);
-}
+} //                            _
 N(sword1) {
-  D(B(T(b) Ta(print, "{") T(o)), //
-    B(T(b) Ta(print, "}") T(o)));
+  D(B(T(b) Ta(print, "a") T(o)), B(T(b) Ta(print, "A") T(o)));
+  D(B(T(b) Ta(print, "b") T(o)), B(T(b) Ta(print, "B") T(o)));
   toti(α, ω, ο, ρ, δ, σ);
-}
+} //                         \nS ->
 int main() {
   long α, ω;
   text_t ο[α = 1024];
   long ρ = 3, δ = 1, σ = 0;
-  ω = B(T(b)                 //
-        Ta(print, "\nS -> ") //
-        T(sword1)            //
-        Ta(print, "M1")      //
-        T(sword2)            //
-        Ta(print, "M2")      //
-        T(o));
+  ω = B(T(b) Ta(print, "\nS -> ") T(sword1) Ta(print, "_") T(sword2) Ta(print, ".") T(o));
   ο[ω - 1].q = ω;
   ο[ω + 1].q = 1;
   BConnect(ω, ω, 1);
