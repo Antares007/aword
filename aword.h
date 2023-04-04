b772ded07d039044871491726a33c801a51a75dd
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-04 04:40:45 +0400

    delete old staff

--- aword.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define N(argo) void argo(void **o, long t, long a, long r, long i)
-#define Ta(nar, val) o[--a] = val, o[--a] = nar, o[--a] = #nar,
-#define T(n) Ta(n, 0)
-typedef N((*w_t));
-
