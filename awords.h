d8d7db77a3945d1192eb27f254f5e208ae0054b4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-06 03:41:58 +0400

    dream in colors

--- awords.h
+++ awords.h
@@ -1,7 +1,7 @@
 #pragma once
 // clang-format off
 #ifndef bWords
-#define OP(a, op, b) a.tin op - (b)
+#define OP(a, op, b) a.tin op - (b) 
 #define Push(s) --s.tin
 #define Pull(s) s.tin++
 #else
@@ -9,34 +9,26 @@
 #define Push(s) s.tin++
 #define Pull(s) --s.tin
 #endif
-#define AW 8
-
-#define D(n, ...)                                         \
-  ({ nar_t arms[][20][2] = {__VA_ARGS__};                 \
-    ο[OP(ω, +, AW - 1)].q = sizeof(arms) / sizeof(*arms); \
-    ο[OP(ω, +, AW)].v = arms; })
-// first Β is unicode greek Beta
-#define Β(...) {__VA_ARGS__, o}}
-#define B(...) {{__VA_ARGS__, o}, Β
-
 #define T(ina, ...) ο[Push(α)].q=0, \
                     ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0, \
-                    ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0, \
                     ο[__VA_ARGS__ Push(α)].v=ina, \
-                    ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0, \
                     ο[Push(α)].q=0,ο[Push(α)].q=0,ο[Push(α)].q=0, \
                     ο[Push(α)].q=0
-#define N(argo) void argo(tin_v α, tin_v ω, struct txt_t *ο, ray_v ρ, dir_v δ, tin_v σ)
+#define Twin(nar) (T(nar, ο[Push(σ)].w.tin =), ο[Pull(σ)].w)
+#define Args win_v α, win_v ω, struct txt_t *ο, ray_v ρ, dir_v δ, win_v σ
+#define N(argo) void argo(Args)
 typedef struct ray_v { long ray; } ray_v;
-typedef struct tin_v { long tin; } tin_v;
+typedef struct win_v { long tin; } win_v;
 typedef struct dir_v { long dir; } dir_v;
 typedef struct txt_t {
-  union { long q; void *v; const char *cs; struct txt_t *t; tin_v w; N((*go)); };
+  union { long q; void *v; const char *cs; struct txt_t *t; win_v w; N((*go)); };
 } txt_t;
 typedef N((*nar_t));
-#define White   (dir_v){+1}
-#define Black   (dir_v){-1}
-#define Yellow  (ray_v){3}
-#define Red     (ray_v){2}
-#define Green   (ray_v){1}
-#define Blue    (ray_v){0}
+#define P_Yellow(w) ο[OP(w, +, 4)]
+#define P_Red(w) ο[OP(w, +, 3)]
+#define P_Green(w) ο[OP(w, +, 2)]
+#define P_Blue(w) ο[OP(w, +, 1)]
+#define P_DarkBlue(w) ο[OP(w, -, 1)]
+#define P_DarkGreen(w) ο[OP(w, -, 2)]
+#define P_DarkRed(w) ο[OP(w, -, 3)]
+#define P_DarkYellow(w) ο[OP(w, -, 4)]
--- os.c
+++ os.c
@@ -5,211 +5,206 @@
 #include <raylib.h>
 #include <stdio.h>
 #include <unistd.h>
-N(m) {
-  ω.tin = OP(ω, +, (AW + 1 + AW) * δ.dir);
+N(go) {
+  ω.tin = OP(ω, +, (1 + 4 + 4) * δ.dir);
   ο[ω.tin].go(α, ω, ο, ρ, δ, σ);
 }
-N(go_White_Yellow) /**/ { m(α, ω, ο, Yellow, /**/ White, σ); }
-N(go_White_Red) /*   */ { m(α, ω, ο, Red, /*   */ White, σ); }
-N(go_White_Green) /* */ { m(α, ω, ο, Green, /* */ White, σ); }
-N(go_White_Blue) /*  */ { m(α, ω, ο, Blue, /*  */ White, σ); }
-N(go_Black_Blue) /*  */ { m(α, ω, ο, Blue, /*  */ Black, σ); };
-N(go_Black_Green) /* */ { m(α, ω, ο, Green, /* */ Black, σ); };
-N(go_Black_Red) /*   */ { m(α, ω, ο, Red, /*   */ Black, σ); };
-N(go_Black_Yellow) /**/ { m(α, ω, ο, Yellow, /**/ Black, σ); };
+N(go_White) { go(α, ω, ο, ρ, (dir_v){+1}, σ); }
+N(go_Black) { go(α, ω, ο, ρ, (dir_v){-1}, σ); }
+N(go_Yellow) /*    */ { go_White(α, ω, ο, (ray_v){3}, δ, σ); }
+N(go_Red) /*       */ { go_White(α, ω, ο, (ray_v){2}, δ, σ); }
+N(go_Green) /*     */ { go_White(α, ω, ο, (ray_v){1}, δ, σ); }
+N(go_Blue) /*      */ { go_White(α, ω, ο, (ray_v){0}, δ, σ); }
+N(go_DarkBlue) /*  */ { go_Black(α, ω, ο, (ray_v){0}, δ, σ); };
+N(go_DarkGreen) /* */ { go_Black(α, ω, ο, (ray_v){1}, δ, σ); };
+N(go_DarkRed) /*   */ { go_Black(α, ω, ο, (ray_v){2}, δ, σ); };
+N(go_DarkYellow) /**/ { go_Black(α, ω, ο, (ray_v){3}, δ, σ); };
 N(aradani_heart) { ο[OP(ω, +, (1 + ρ.ray) * δ.dir)].go(α, ω, ο, ρ, δ, σ); }
-N(b) { m(α, ω, ο, ρ, White, σ); }
+N(b) { go_White(α, ω, ο, ρ, δ, σ); }
 N(o) {
-  ο[OP(ω, +, +1 + 3 /* White Yellow */)].v = go_Black_Green;
-  ο[OP(ω, +, +1 + 2 /* White Red    */)].v = go_Black_Red;
-  ο[OP(ω, +, +1 + 1 /* White Green  */)].v = go_Black_Yellow;
-  ο[OP(ω, +, +1 + 0 /* White Blue   */)].v = go_Black_Blue;
+  P_Yellow /**/ (ω).v = go_DarkGreen;
+  P_Red /*   */ (ω).v = go_DarkRed;
+  P_Green /* */ (ω).v = go_DarkYellow;
+  P_Blue /*  */ (ω).v = go_DarkBlue;
   ο[ω.tin].v = aradani_heart;
   aradani_heart(α, ω, ο, ρ, δ, σ);
 }
-#define P(ω, n) ο[OP(ω, +, AW - n)]
 
 N(toti_arm_go_White_w) {
-  tin_v Tw = P(ω, 0).w;
-  tin_v next_w = P(ω, 1).w;
-  P(Tw, 2).w = next_w;
-  m(α, Tw, ο, ρ, White, σ);
+  win_v Tw = P_Yellow(ω).w;
+  win_v next_w = P_DarkYellow(ω).w;
+  P_Red(Tw).w = next_w;
+  go_White(α, Tw, ο, ρ, δ, σ);
 }
 N(toti_arm_go_Black_w) {
-  tin_v Tw = P(ω, 0).w;
-  P(Tw, 2).w = P(Tw, 3).w;
-  m(α, Tw, ο, ρ, δ, σ);
+  win_v Tw = P_Yellow(ω).w;
+  P_Red(Tw).w = P_DarkRed(Tw).w;
+  go(α, Tw, ο, ρ, δ, σ);
 }
-N(toti_arm_go_Nxt_arm) { m(α, P(ω, 0).w, ο, ρ, White, σ); }
-N(toti_heart) { m(α, P(ω, 2).w, ο, ρ, White, σ); }
+N(toti_arm_go_Nxt_arm) { go_White(α, P_Yellow(ω).w, ο, ρ, δ, σ); }
+
+N(toti_heart) { go_White(α, P_Red(ω).w, ο, ρ, δ, σ); }
 N(toti) {
-  nar_t **arms = P(ω, 0).v;
-  long c = P(ω, 1).q;
 #define WriteArm                                                               \
   c--;                                                                         \
   while (*arms[c] != o)                                                        \
     T(*arms[c]), arms[c]++;                                                    \
   T(*arms[c]);
-  tin_v r, l;
-  T(toti_arm_go_Black_w, l.tin =), P(l, 0).w = ω;
+  nar_t **arms = P_Blue(ω).v;
+  long c = P_DarkBlue(ω).q;
+  win_v r, l;
+  l = Twin(toti_arm_go_Black_w), P_Yellow(l).w = ω;
   WriteArm;
-  T(toti_arm_go_White_w, r.tin =), P(r, 0).w = ω, P(r, 1).w = l;
+  r = Twin(toti_arm_go_White_w), P_Yellow(r).w = ω, P_DarkYellow(r).w = l;
   WriteArm;
   while (c) {
-    T(toti_arm_go_Nxt_arm, l.tin =), P(l, 0).w = r;
+    l = Twin(toti_arm_go_Nxt_arm), P_Yellow(l).w = r;
     WriteArm;
-    T(toti_arm_go_White_w, r.tin =), P(r, 0).w = ω, P(r, 1).w = l;
+    r = Twin(toti_arm_go_White_w), P_Yellow(r).w = ω, P_DarkYellow(r).w = l;
     WriteArm;
   }
-  P(ω, 2).w = r;
-  P(ω, 3).w = r;
+  P_Red(ω).w = r;
+  P_DarkRed(ω).w = r;
   ο[ω.tin].v = toti_heart;
   toti_heart(α, ω, ο, ρ, δ, σ);
 #undef WriteArm
 }
-N(id1) {
-  printf("id1\n");
-  m(α, ω, ο, ρ, δ, σ);
-}
-N(id2) {
-  printf("id2\n");
-  m(α, ω, ο, ρ, δ, σ);
-}
+N(id1) { DrawText("Right", 300, 400, 45, RED), go(α, ω, ο, ρ, δ, σ); }
+N(id2) { DrawText("Left", 400, 400, 45, YELLOW), go(α, ω, ο, ρ, δ, σ); }
 N(us) {
   nar_t *arms[] = {(nar_t[]){id1, o}, (nar_t[]){id2, o}};
-  P(ω, 0).v = arms;
-  P(ω, 1).q = sizeof(arms) / sizeof(*arms);
+  P_Blue(ω).v = arms;
+  P_DarkBlue(ω).q = sizeof(arms) / sizeof(*arms);
   toti(α, ω, ο, ρ, δ, σ);
 }
-
 N(not ) {
-  ο[OP(ω, +, +1 + 3 /* White Yellow */)].v = go_Black_Green;
-  ο[OP(ω, +, +1 + 2 /* White Red    */)].v = go_White_Yellow;
-  ο[OP(ω, +, +1 + 1 /* White Green  */)].v = go_Black_Yellow;
-  ο[OP(ω, +, +1 + 0 /* White Blue   */)].v = go_White_Blue;
+  P_Yellow /*     */ (ω).v = go_DarkGreen;
+  P_Red /*        */ (ω).v = go_Yellow;
+  P_Green /*      */ (ω).v = go_DarkYellow;
+  P_Blue /*       */ (ω).v = go_Blue;
   ο[ω.tin].v = aradani_heart;
-  ο[OP(ω, +, -1 - 0 /* Black Blue   */)].v = go_Black_Blue;
-  ο[OP(ω, +, -1 - 1 /* Black Green  */)].v = go_White_Green;
-  ο[OP(ω, +, -1 - 2 /* Black Red    */)].v = go_Black_Red;
-  ο[OP(ω, +, -1 - 3 /* Black Yellow */)].v = go_Black_Yellow;
+  P_DarkBlue /*   */ (ω).v = go_DarkBlue;
+  P_DarkGreen /*  */ (ω).v = go_Green;
+  P_DarkRed /*    */ (ω).v = go_DarkRed;
+  P_DarkYellow /* */ (ω).v = go_DarkYellow;
   aradani_heart(α, ω, ο, ρ, δ, σ);
 }
 N(and) {
-  ο[OP(ω, +, +1 + 3 /* White Yellow */)].v = go_Black_Green;
-  ο[OP(ω, +, +1 + 2 /* White Red    */)].v = go_White_Red;
-  ο[OP(ω, +, +1 + 1 /* White Green  */)].v = go_White_Yellow;
-  ο[OP(ω, +, +1 + 0 /* White Blue   */)].v = go_White_Blue;
+  P_Yellow /*     */ (ω).v = go_DarkGreen;
+  P_Red /*        */ (ω).v = go_Red;
+  P_Green /*      */ (ω).v = go_Yellow;
+  P_Blue /*       */ (ω).v = go_Blue;
   ο[ω.tin].v = aradani_heart;
-  ο[OP(ω, +, -1 - 0 /* Black Blue   */)].v = go_Black_Blue;
-  ο[OP(ω, +, -1 - 1 /* Black Green  */)].v = go_White_Green;
-  ο[OP(ω, +, -1 - 2 /* Black Red    */)].v = go_Black_Red;
-  ο[OP(ω, +, -1 - 3 /* Black Yellow */)].v = go_Black_Yellow;
+  P_DarkBlue /*   */ (ω).v = go_DarkBlue;
+  P_DarkGreen /*  */ (ω).v = go_Green;
+  P_DarkRed /*    */ (ω).v = go_DarkRed;
+  P_DarkYellow /* */ (ω).v = go_DarkYellow;
   aradani_heart(α, ω, ο, ρ, δ, σ);
 }
 N(or) {
-  ο[OP(ω, +, +1 + 3 /* White Yellow */)].v = go_Black_Green;
-  ο[OP(ω, +, +1 + 2 /* White Red    */)].v = go_White_Red;
-  ο[OP(ω, +, +1 + 1 /* White Green  */)].v = go_Black_Yellow;
-  ο[OP(ω, +, +1 + 0 /* White Blue   */)].v = go_White_Yellow;
+  P_Yellow /*     */ (ω).v = go_DarkGreen;
+  P_Red /*        */ (ω).v = go_Red;
+  P_Green /*      */ (ω).v = go_DarkYellow;
+  P_Blue /*       */ (ω).v = go_Yellow;
   ο[ω.tin].v = aradani_heart;
-  ο[OP(ω, +, -1 - 0 /* Black Blue   */)].v = go_Black_Blue;
-  ο[OP(ω, +, -1 - 1 /* Black Green  */)].v = go_White_Green;
-  ο[OP(ω, +, -1 - 2 /* Black Red    */)].v = go_Black_Red;
-  ο[OP(ω, +, -1 - 3 /* Black Yellow */)].v = go_Black_Yellow;
+  P_DarkBlue /*   */ (ω).v = go_DarkBlue;
+  P_DarkGreen /*  */ (ω).v = go_Green;
+  P_DarkRed /*    */ (ω).v = go_DarkRed;
+  P_DarkYellow /* */ (ω).v = go_DarkYellow;
   aradani_heart(α, ω, ο, ρ, δ, σ);
 }
 N(orand) {
-  ο[OP(ω, +, +1 + 3 /* White Yellow */)].v = go_Black_Green;
-  ο[OP(ω, +, +1 + 2 /* White Red    */)].v = go_White_Red;
-  ο[OP(ω, +, +1 + 1 /* White Green  */)].v = go_White_Yellow;
-  ο[OP(ω, +, +1 + 0 /* White Blue   */)].v = go_White_Yellow;
+  P_Yellow /*     */ (ω).v = go_DarkGreen;
+  P_Red /*        */ (ω).v = go_Red;
+  P_Green /*      */ (ω).v = go_Yellow;
+  P_Blue /*       */ (ω).v = go_Yellow;
   ο[ω.tin].v = aradani_heart;
-  ο[OP(ω, +, -1 - 0 /* Black Blue   */)].v = go_Black_Blue;
-  ο[OP(ω, +, -1 - 1 /* Black Green  */)].v = go_White_Green;
-  ο[OP(ω, +, -1 - 2 /* Black Red    */)].v = go_Black_Red;
-  ο[OP(ω, +, -1 - 3 /* Black Yellow */)].v = go_Black_Yellow;
+  P_DarkBlue /*   */ (ω).v = go_DarkBlue;
+  P_DarkGreen /*  */ (ω).v = go_Green;
+  P_DarkRed /*    */ (ω).v = go_DarkRed;
+  P_DarkYellow /* */ (ω).v = go_DarkYellow;
   aradani_heart(α, ω, ο, ρ, δ, σ);
 }
 
 static Font font;
 N(begindrawing) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir)
+  if (ρ.ray == 1 && δ.dir == 1)
     BeginDrawing(), ClearBackground(BLACK);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 };
 N(enddrawing) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir)
+  if (ρ.ray == 1 && δ.dir == 1)
     EndDrawing();
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 };
 N(the_end) {
-  if (ρ.ray != Green.ray || δ.dir != White.dir)
-    m(α, ω, ο, ρ, δ, σ);
+  if (ρ.ray != 1 || δ.dir != 1)
+    go(α, ω, ο, ρ, δ, σ);
 }
 N(fps) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir)
+  if (ρ.ray == 1 && δ.dir == 1)
     DrawTextEx(font, TextFormat("%d", GetFPS()), (Vector2){0, 0}, 25, 4, WHITE);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 }
 N(shouldclose) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir)
-    m(α, ω, ο, (ray_v){!!WindowShouldClose()}, δ, σ);
+  if (ρ.ray == 1 && δ.dir == 1)
+    go(α, ω, ο, (ray_v){!!WindowShouldClose()}, δ, σ);
   else
-    m(α, ω, ο, ρ, δ, σ);
+    go(α, ω, ο, ρ, δ, σ);
 }
 N(getcharpressed) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir) {
+  if (ρ.ray == 1 && δ.dir == 1) {
     long key = GetCharPressed();
     if (key)
-      ο[OP(ω, +, 1)].q = key;
-    if (ο[OP(ω, +, 1)].q) {
-      ο[Push(σ)] = ο[OP(ω, +, 1)];
-      m(α, ω, ο, Green, δ, σ);
+      P_Yellow(ω).q = key;
+    if (P_Yellow(ω).q) {
+      ο[Push(σ)] = P_Yellow(ω);
+      go_Green(α, ω, ο, ρ, δ, σ);
     } else
-      m(α, ω, ο, Blue, δ, σ);
+      go_Blue(α, ω, ο, ρ, δ, σ);
   } else
-    m(α, ω, ο, ρ, δ, σ);
+    go(α, ω, ο, ρ, δ, σ);
 }
 N(write_char) {
-  if (ρ.ray == Green.ray && δ.dir == White.dir)
+  if (ρ.ray == 1 && δ.dir == 1)
     DrawTextEx(font, TextFormat("%ld", ο[Pull(σ)]), (Vector2){200, 100}, 45, 4,
                WHITE);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 }
 N(left) {
   DrawText("left", 200, 0, 45, YELLOW);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 }
 N(right) {
   DrawText("right", 400, 0, 45, RED);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 }
 N(print) {
   DrawText("heyy", 200, 200, 45, RED);
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
 }
 int main() {
-  tin_v α = {1024}, ω;
+  win_v α = {1024}, ω;
   txt_t β[α.tin + 1 + α.tin];
   txt_t *ο = β + α.tin;
   // α.tin = 0 - α.tin;
-  ray_v ρ = Yellow;
-  dir_v δ = White;
-  tin_v σ = {0};
-  T(b, ω.tin =);
+  ray_v ρ = {3};
+  dir_v δ = {1};
+  win_v σ = {0};
+  ω = Twin(b);
   T(begindrawing);
   T(fps), T(print);
   T(and), T(us);
   T(orand), T(shouldclose), T(the_end), T(or), T(enddrawing);
   T(o);
   assert(ο[OP(ω, +, 0)].v == b);
-  assert(ο[OP(ω, +, 17)].v == begindrawing);
+  assert(ο[OP(ω, +, 1 + 4 + 4)].v == begindrawing);
 
   SetTraceLogLevel(LOG_ERROR);
   SetTargetFPS(60);
   InitWindow(1500, 900, "aword os");
   font = LoadFont("NovaMono-Regular.ttf");
-  m(α, ω, ο, ρ, δ, σ);
+  go(α, ω, ο, ρ, δ, σ);
   CloseWindow();
   return 0;
 }
