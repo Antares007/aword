05509b0cda6908f54d6e8bb7349cd1fbad8cea9d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-08 15:56:22 +0400

    add notandor

--- aword.c
+++ aword.c
@@ -41,3 +41,60 @@ int main() {
   void *text[] = {T(b), T(toti), T(r), T(o)};
   m(text + 5, 3, 1);
 }
+N(go_Yellow) /*    */ {}
+N(go_Red) /*       */ {}
+N(go_Green) /*     */ {}
+N(go_Blue) /*      */ {}
+N(go_DarkBlue) /*  */ {}
+N(go_DarkGreen) /* */ {}
+N(go_DarkRed) /*   */ {}
+N(go_DarkYellow) /**/ {}
+N(aradani_pith) {}
+N(not) {
+  τ[+1 + 3] = /*Yellow*/ go_DarkGreen;
+  τ[+1 + 2] = /*Red   */ go_Yellow;
+  τ[+1 + 1] = /*Green */ go_DarkYellow;
+  τ[+1 + 0] = /*Blue  */ go_DarkBlue;
+  τ[0] = aradani_pith;
+  τ[-1 - 0] = /*DarkBlue  */ go_DarkBlue;
+  τ[-2 - 1] = /*DarkGreen */ go_Green;
+  τ[-3 - 2] = /*DarkRed   */ go_DarkRed;
+  τ[-4 - 3] = /*DarkYellow*/ go_DarkYellow;
+  aradani_pith(τ, ρ, δ);
+}
+N(and) {
+  τ[+1 + 3] = /*Yellow*/ go_DarkGreen;
+  τ[+1 + 2] = /*Red   */ go_Red;
+  τ[+1 + 1] = /*Green */ go_Yellow;
+  τ[+1 + 0] = /*Blue  */ go_Blue;
+  τ[0] = aradani_pith;
+  τ[-1 - 0] = /*DarkBlue  */ go_DarkBlue;
+  τ[-2 - 1] = /*DarkGreen */ go_Green;
+  τ[-3 - 2] = /*DarkRed   */ go_DarkRed;
+  τ[-4 - 3] = /*DarkYellow*/ go_DarkYellow;
+  aradani_pith(τ, ρ, δ);
+}
+N(or) {
+  τ[+1 + 3] = /*Yellow*/ go_DarkGreen;
+  τ[+1 + 2] = /*Red   */ go_Red;
+  τ[+1 + 1] = /*Green */ go_DarkYellow;
+  τ[+1 + 0] = /*Blue  */ go_Yellow;
+  τ[0] = aradani_pith;
+  τ[-1 - 0] = /*DarkBlue  */ go_DarkBlue;
+  τ[-2 - 1] = /*DarkGreen */ go_Green;
+  τ[-3 - 2] = /*DarkRed   */ go_DarkRed;
+  τ[-4 - 3] = /*DarkYellow*/ go_DarkYellow;
+  aradani_pith(τ, ρ, δ);
+}
+N(orand) {
+  τ[+1 + 3] = /*Yellow*/ go_DarkGreen;
+  τ[+1 + 2] = /*Red   */ go_Red;
+  τ[+1 + 1] = /*Green */ go_Yellow;
+  τ[+1 + 0] = /*Blue  */ go_Yellow;
+  τ[0] = aradani_pith;
+  τ[-1 - 0] = /*DarkBlue  */ go_DarkBlue;
+  τ[-2 - 1] = /*DarkGreen */ go_Green;
+  τ[-3 - 2] = /*DarkRed   */ go_DarkRed;
+  τ[-4 - 3] = /*DarkYellow*/ go_DarkYellow;
+  aradani_pith(τ, ρ, δ);
+}
