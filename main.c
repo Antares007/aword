11709476b11ce7ce5851d44ed02cb7d096c44ff4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-10 19:49:06 +0400

    renames

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-#define P printf("%5s %s\n", (char *)b[t].v, __FUNCTION__), usleep(200000)
-#define N(argo) void argo(struct base_t *b, long a, long t, long o)
+#define P printf("%5s %s\n", (char *)ο[τ].v, __FUNCTION__), usleep(100000)
+#define N(argo) void argo(long τ, long α, long β, struct base_t *ο, long σ)
+//    τ
+// bnnna....owwww
+//
+//          ατ
+//      ___.wwb.wwb
+// ονννσ
+//      ___.wwb.wwb
+//
 typedef struct base_t {
   union {
     long q;
@@ -11,85 +19,70 @@ typedef struct base_t {
 } base_t;
 typedef N((*n_t));
 // clang-format off
-N(Yellow    ) { P, b[t + 5].c(b, a, t + 9, o); }
-N(Red       ) { P, b[t + 6].c(b, a, t + 9, o); }
-N(Green     ) { P, b[t + 7].c(b, a, t + 9, o); }
-N(Blue      ) { P, b[t + 8].c(b, a, t + 9, o); }
-N(DarkBlue  ) { P, b[t - 8].c(b, a, t - 9, o); }
-N(DarkGreen ) { P, b[t - 7].c(b, a, t - 9, o); }
-N(DarkRed   ) { P, b[t - 6].c(b, a, t - 9, o); }
-N(DarkYellow) { P, b[t - 5].c(b, a, t - 9, o); }
-N(grow      ) { b[a - 1].c(b, a - 1, t, o); }
-N(s) {
-  b[--o].v = Yellow, b[o].v = Yellow, b[o].v = Yellow;
-  b[--o].v = Red,    b[o].v = Red,    b[o].v = Red;
-  b[--o].v = Green,  b[o].v = Green,  b[o].v = Green;
-  b[--o].v = Blue,   b[o].v = Blue,   b[o].v = Blue;
-  b[t=--o].v = "s";
-  b[--o].v = Blue,   b[o].v = Blue,   b[o].v = Blue;
-  b[--o].v = Green,  b[o].v = Green,  b[o].v = Green;
-  b[--o].v = Red,    b[o].v = Red,    b[o].v = Red;
-  b[--o].v = Yellow, b[o].v = Yellow, b[o].v = Yellow;
-  Yellow(b, a, t, o);
+N(Yellow    ) { P, ο[τ + 5].c(τ + 9, α, β, ο, σ); }
+N(Red       ) { P, ο[τ + 6].c(τ + 9, α, β, ο, σ); }
+N(Green     ) { P, ο[τ + 7].c(τ + 9, α, β, ο, σ); }
+N(Blue      ) { P, ο[τ + 8].c(τ + 9, α, β, ο, σ); }
+N(DarkBlue  ) { P, ο[τ - 8].c(τ - 9, α, β, ο, σ); }
+N(DarkGreen ) { P, ο[τ - 7].c(τ - 9, α, β, ο, σ); }
+N(DarkRed   ) { P, ο[τ - 6].c(τ - 9, α, β, ο, σ); }
+N(DarkYellow) { P, ο[τ - 5].c(τ - 9, α, β, ο, σ); }
+N(grow      ) { ο[σ - 1].c(τ, α, β, ο, σ - 1); }
+N(tab) {
+  ο[β*0+--α].v = Yellow, ο[β*1+α].v = Yellow, ο[β*2+α].v = Yellow;
+  ο[β*0+--α].v = Red,    ο[β*1+α].v = Red,    ο[β*2+α].v = Red;
+  ο[β*0+--α].v = Green,  ο[β*1+α].v = Green,  ο[β*2+α].v = Green;
+  ο[β*0+--α].v = Blue,   ο[β*1+α].v = Blue,   ο[β*2+α].v = Blue;
+  ο[τ=β*0+--α].v = "tab0";
+  ο[  β*1+--α].v = "tab1";
+  ο[  β*2+--α].v = "tab2";
+  ο[β*0+--α].v = Blue,   ο[β*1+α].v = Blue,   ο[β*2+α].v = Blue;
+  ο[β*0+--α].v = Green,  ο[β*1+α].v = Green,  ο[β*2+α].v = Green;
+  ο[β*0+--α].v = Red,    ο[β*1+α].v = Red,    ο[β*2+α].v = Red;
+  ο[β*0+--α].v = Yellow, ο[β*1+α].v = Yellow, ο[β*2+α].v = Yellow;
+  Yellow(τ, α, β, ο, σ);
 }
-N(d) {
-  b[--o].v = "d";
-  b[--o].v = DarkBlue;
-  b[--o].v = DarkYellow;
-  b[--o].v = DarkRed;
-  b[--o].v = DarkGreen;
-  grow(b, a, t, o);
+N(dot) {
+  ο[--α].v = "dot";
+  ο[--α].v = DarkBlue;
+  ο[--α].v = DarkYellow;
+  ο[--α].v = DarkRed;
+  ο[--α].v = DarkGreen;
+  grow(τ, α, β, ο, σ);
 }
-N(sd) {
-  b[--o].v = d;
-  b[--o].v = Red;
-  b[--o].v = Green;
-  b[--o].v = Blue;
-  b[t = --o].v = "sd";
-  b[--o].v = Blue;
-  b[--o].v = Green;
-  b[--o].v = Red;
-  b[--o].v = Yellow;
-  Yellow(b, a, t, o);
+N(toti) {
+  ο[--α].v = dot;
+  ο[--α].v = Red;
+  ο[--α].v = Green;
+  ο[--α].v = Blue;
+  ο[τ = --α].v = "toti";
+  ο[--α].v = Blue;
+  ο[--α].v = Green;
+  ο[--α].v = Red;
+  ο[--α].v = Yellow;
+  Yellow(τ, α, β, ο, σ);
 }
-N(wor) {
-  b[--o].v = DarkYellow;
-  b[--o].v = DarkRed;
-  b[--o].v = DarkGreen;
-  b[--o].v = DarkBlue;
-  b[--o].v = "wor";
-  b[--o].v = Blue;
-  b[--o].v = Green;
-  b[--o].v = Red;
-  b[--o].v = Yellow;
-  grow(b, a, t, o);
-}
-N(love_Green_heart) {
-  b[a++].v = s;
-  b[a++].v = d;
-  grow(b, a, t, o);
-}
-N(love) {
-  b[--o].v = DarkYellow;
-  b[--o].v = DarkRed;
-  b[--o].v = DarkGreen;
-  b[--o].v = DarkBlue;
-  b[--o].v = "love";
-  b[--o].v = Blue;
-  b[--o].v = love_Green_heart;
-  b[--o].v = Red;
-  b[--o].v = Yellow;
-  grow(b, a, t, o);
+N(word) {
+  ο[--α].v = DarkYellow;
+  ο[--α].v = DarkRed;
+  ο[--α].v = DarkGreen;
+  ο[--α].v = DarkBlue;
+  ο[--α].v = "word";
+  ο[--α].v = Blue;
+  ο[--α].v = Green;
+  ο[--α].v = Red;
+  ο[--α].v = Yellow;
+  grow(τ, α, β, ο, σ);
 }
 int main() {
-  long t;
-  base_t b[1024];
-  long a = 0, o = sizeof(b) / sizeof(*b);
-  b[a++].v = s;
-  b[a++].v = wor;
-  b[a++].v = sd;
-  b[a++].v = love;
-  b[a++].v = d;
-  grow(b, a, t, o);
+  long τ, σ = 0;
+  base_t ο[1024];
+  long α = sizeof(ο) / sizeof(*ο), β = 0;
+  ο[σ++].v = tab;
+  ο[σ++].v = word;
+  ο[σ++].v = toti;
+  ο[σ++].v = word;
+  ο[σ++].v = dot;
+  grow(τ, α, β, ο, σ);
   return 0;
 }
