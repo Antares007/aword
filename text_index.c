00c1d58d58bf5c0d81c7e2449b0066c1fcba02de
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-04-14 05:55:41 +0400

    propeller

--- text_index.c
+++ text_index.c
@@ -8,26 +8,26 @@
 
 static Font font;
 static Color colors[] = {
-    {255, 255, 0, 255}, // Yellow
-    {128, 0, 128, 255}, // Purple
-    {255, 0, 0, 255},   // Red
-    {0, 128, 0, 255},   // Green
-    {0, 0, 255, 255},   // Blue
-    {0, 0, 0, 255},     // Black
-    {0, 0, 128, 255},   // Navy
-    {0, 255, 0, 255},   // Lime
-    {128, 0, 0, 255},   // Maroon
-    {255, 0, 255, 255}, // Fuchsia
-    {128, 128, 0, 255}  // Olive
+    {255, 255, 000, 255}, // Yellow
+    {128, 000, 128, 255}, // Purple
+    {255, 000, 000, 255}, // Red
+    {000, 128, 000, 255}, // Green
+    {000, 000, 255, 255}, // Blue
+    {000, 000, 000, 255}, // Black
+    {000, 000, 128, 255}, // Navy
+    {000, 255, 000, 255}, // Lime
+    {128, 000, 000, 255}, // Maroon
+    {255, 000, 255, 255}, // Fuchsia
+    {128, 128, 000, 255}  // Olive
 
 };
-static void**o;
+static void **o;
 static long tabs[1024][3];
 static long tc = 0;
-static float zoom = 4;
+static float zoom = 2;
 static Vector2 off = {};
 static void drAW(long t, long r) {
-  DrawRectangleLines(-10,-10,20,20,BLACK);
+  DrawRectangleLines(-10, -10, 20, 20, BLACK);
 }
 static void draw() {
   int key = GetCharPressed();
@@ -45,14 +45,15 @@ static void draw() {
     BeginDrawing();
     ClearBackground(WHITE);
     Camera2D camera = {.target = off, .rotation = 0, .zoom = zoom};
-    camera.offset = (Vector2){GetScreenWidth() / 2.f, GetScreenHeight() / 2.f};
+    camera.offset = (Vector2){GetScreenWidth() / 4.f, GetScreenHeight() / 8.f};
     Vector2 startPos = {0, 0};
     for (long i = 0; i < tc; i++) {
       BeginMode2D(camera);
-      void*ptr = o[tabs[i][2]+tabs[i][0]];
-      const char *text = TextFormat("%ld %s", tabs[i][1], ptr<(void*)2048?"":ptr);
-      DrawTextEx(font, text, (Vector2){-100, i * 10 - 100}, 10, 0, BLACK);
-      Vector2 endPos = {tabs[i][0] * 5, tabs[i][1] * 5};
+      void *ptr = o[tabs[i][2] + tabs[i][0]];
+      const char *text =
+          TextFormat("%ld %s", tabs[i][1], ptr < (void *)2048 ? "" : ptr);
+      long axis = tabs[i][2] / 512;
+      Vector2 endPos = {tabs[i][0] * 5, tabs[i][1] * 5 + 100 * axis};
       DrawTextEx(font, text, endPos, 10, 0, colors[tabs[i][1] + 5]);
       DrawLineBezier(startPos, endPos, 2, colors[tabs[i][1] + 5]);
       startPos = endPos;
@@ -66,7 +67,7 @@ void ti(Args, long ray) {
   tabs[tc][1] = ray - τ;
   tabs[tc][2] = β;
   tc++;
-  draw(), ((n_t*)ο)[β + ray](τ, α, β, ο, σ);
+  draw(), ((n_t *)ο)[β + ray](τ, α, β, ο, σ);
 }
 void ti_init(Args) {
   o = ο;
