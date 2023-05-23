3800b5bafe026232f6ccedff4fb4e37b19b54d32
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-05-24 00:34:57 +0400

    gamovida

--- src/sshow.c
+++ src/sshow.c
@@ -1,5 +1,5 @@
-#define White(op) (d op +3)
-#define Black(op) (d op -3)
+#define White(op) (d op + 3)
+#define Black(op) (d op - 3)
 #define Yellow(op) (r op 3)
 #define Red(op) (r op 2)
 #define Green(op) (r op 1)
@@ -21,14 +21,21 @@ typedef N((*aword_t));
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
-#define Log printf("%2ld %ld %s \n", d, r, __FUNCTION__), usleep(3000)
-N(m)    { ((aword_t *)o)[w + d](t, a, w + d, o, r, d, s); }
-N(b)    { (o[0].q = 1), m(t, a, w, o, r, +3, s); }
-N(dot)  { m(t, a, w, o, r == 3 ? 1 : r == 1 ? 3 : r, -3, s); }
-N(A)    { if(Green(==)&&White(==))Log; m(t, a, w, o, r, d, s); }
-N(B)    { if(Green(==)&&White(==))Log; m(t, a, w, o, r, d, s); }
-N(C)    { if(Green(==)&&White(==))Log; m(t, a, w, o, r, d, s); }
-N(D)    { if(Green(==)&&White(==))Log; m(t, a, w, o, r, d, s); }
+#define Log printf("%s", __FUNCTION__), usleep(30000)
+N(m) { ((aword_t *)o)[w + d](t, a, w + d, o, r, d, s); }
+N(b) { (o[0].q = 1), m(t, a, w, o, r, +3, s); }
+N(dot) { m(t, a, w, o, r == 3 ? 1 : r == 1 ? 3 : r, -3, s); }
+N(A) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(B) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(O) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_0) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_1) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_2) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_3) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_4) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_5) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_6) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
+N(_7) { if (Green(==) && White(==)) Log; m(t, a, w, o, r, d, s); }
 
 N(print) {
   if (r == 1 && d == +3)
@@ -36,34 +43,18 @@ N(print) {
   m(t, a, w, o, r, d, s);
 }
 N(tab);
-N(toti_heart) {
-  Log;
-  if (r == 1) {
-    printf("wtf\n");
-    o[--s].q = r;
-    o[--s].q = d;
-    o[--s].q = w;
-    m(t, a, o[w - 1].q, o, 3, 3, s);
-  } else
-    m(t, a, w, o, r, d, s);
-}
-N(toti) {
-  Log;
-  o[w - 1].q = (a + 1);
-  o[w].c = toti_heart;
-  T(tab), T(C), T(dot);
-  toti_heart(t, a, w, o, r, d, s);
-}
 N(term) {
   Log;
-  if (r == 1 && d == 3 && o[t - 1].cs[0] == o[w + 1].cs[0])
+  if (Green(==) && White(==) && o[t - 1].cs[0] == o[w + 1].cs[0])
     o[t - 1].cs++, m(t, a, w, o, r, d, s);
   else
     m(t, a, w, o, r, d, s);
 }
 N(tab) {
-  if (Yellow(==)) m(t, a, o[s + 0].q, o, o[s + 2].q, o[s + 1].q, s + 3);
-  else m(t, a, w, o, r, +3, s);
+  if (Yellow(==))
+    m(t, a, o[s + 0].q, o, o[s + 2].q, o[s + 1].q, s + 3);
+  else
+    m(t, a, w, o, r, +3, s);
 }
 N(var_heart) {
   if (Green(==) && Black(==)) {
@@ -83,18 +74,26 @@ N(var_heart) {
 }
 N(S) {
   Log;
-  long p1 = (a + 1); T(tab), T(A), //Ta(term, "b"),
-        T(dot);
-  long p2 = (a + 1); T(tab), T(B), //T(S), Ta(term, "a"),
-        T(dot);
-  long p3 = (a + 1); T(tab), T(C), //T(S), Ta(term, "a"),
-        T(dot);
-  o[w - 1].v = (long[]){3, 0, p1, p2, p3} + 2; 
+  long p0 = (a + 1);
+  T(tab), T(_0), T(dot);
+  long p1 = (a + 1);
+  T(tab), T(_1), T(dot);
+  long p2 = (a + 1);
+  T(tab), T(_2), T(dot);
+  long p3 = (a + 1);
+  T(tab), T(_3), T(dot);
+  long p4 = (a + 1);
+  T(tab), T(_4), T(dot);
+  long p5 = (a + 1);
+  T(tab), T(_5), T(dot);
+  long p6 = (a + 1);
+  T(tab), T(_6), T(dot);
+  long p7 = (a + 1);
+  T(tab), T(_7), T(dot);
+  o[w - 1].v = (long[]){8, 0, p0, p1, p2, p3, p4, p5, p6, p7} + 2;
   o[w].c = var_heart;
-  var_heart(t,a,w,o,r,d,s);
+  var_heart(t, a, w, o, r, d, s);
 }
-
-// (({a}s)t)
 N(noun) {
   if (r == 1 && d == -3)
     o[t++].v = o[w + 1].v;
@@ -112,6 +111,7 @@ int main() {
   long r = 3, d = 3, s = 512;
   long w = a + 1;
   long t = 256;
-  T(b), Ta(noun, "baaa"), T(S), T(S), T(S), T(print_s), T(dot), m(t, a, w, o, r, d, s);
+  T(b), Ta(noun, " baaa"), T(S), T(S), T(S), T(S), T(print_s), T(dot),
+      m(t, a, w, o, r, d, s);
   return 0;
 }
