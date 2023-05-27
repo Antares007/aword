3d27c8af97765ac1311a9e97838d7a092d53b7e9
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-05-28 03:57:30 +0400

    shiv

--- light/sshow.c
+++ light/sshow.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <unistd.h>
 N(m);
-N(b) { m(a, w, o, r, White(=), s); }
-N(dot) { m(a, w, o, r == 3 ? 1 : r == 1 ? 3 : r, Black(=), s); }
+N(b) { (o[0].q = 1), m(a, w, o, r, White(=), s); }
+N(dot) { (o[0].q = 1), m(a, w, o, r == 3 ? 1 : r == 1 ? 3 : r, Black(=), s); }
 N(tina) { m(a, w, o, r, d, s); }
 N(kargi) { m(a, w, o, r, d, s); }
 N(gogoa) { m(a, w, o, r, d, s); }
@@ -16,26 +16,43 @@ N(id) { m(a, w, o, r, d, s); }
     }                                                                          \
     m(a, w, o, r, d, s);                                                       \
   }
-#define B(ark, ...) (ark = a + 2), T(tab), __VA_ARGS__, T(dot);
+#define B(ark, ...)                                                            \
+  (o[ark + o[ark + 6].q++].q = a + 2), T(tab), __VA_ARGS__, T(dot);
 #define D(ef, ...)                                                             \
   N(ef) {                                                                      \
-    long Olive = 0, Maroon = 0, Lime = 0, Navy = 0, Blue = 0, Green = 0,       \
-         Red = 0, Yellow = 0;                                                  \
+    long Olive    = (a     );                                                  \
+    long Maroon   = (a += 7);                                                  \
+    long Lime     = (a += 7);                                                  \
+    long Navy     = (a += 7);                                                  \
+                    (a += 7);                                                  \
+    long Blue     = (a += 7);                                                  \
+    long Green    = (a += 7);                                                  \
+    long Red      = (a += 7);                                                  \
+    long Yellow   = (a += 7);                                                  \
+    o[Olive +5].q = 0, o[Olive +6].q = 0;                                      \
+    o[Maroon+5].q = 0, o[Maroon+6].q = 0;                                      \
+    o[Lime  +5].q = 0, o[Lime  +6].q = 0;                                      \
+    o[Navy  +5].q = 0, o[Navy  +6].q = 0;                                      \
+    o[Blue  +5].q = 0, o[Blue  +6].q = 0;                                      \
+    o[Green +5].q = 0, o[Green +6].q = 0;                                      \
+    o[Red   +5].q = 0, o[Red   +6].q = 0;                                      \
+    o[Yellow+5].q = 0, o[Yellow+6].q = 0;                                      \
     __VA_ARGS__;                                                               \
-    o[w - 1].v = (long[]){                                                     \
-        Olive, Maroon, Lime, Navy, 0, Blue, Green, Red, Yellow,                \
-    };                                                                         \
+    o[w - 1].q = Olive;                                                        \
     o[w].c = def_heart;                                                        \
     def_heart(a, w, o, r, d, s);                                               \
   }
 N(def_heart) {
-  long *defs = o[w - 1].v;
-  long ray = defs[(r + 1) * d / TW + 4];
-  if (ray) {
-    o[ray + 1].q = w;
-    o[ray - 1].q = r;
-    o[ray - 2].q = d;
-    m(a, ray, o, Yellow(=), White(=), s);
+  long Olive = o[w - 1].q + ((r + 1) * d / TW + 4) * 7;
+  if (o[Olive + 6].q) {
+    long bti        = o[Olive + o[Olive + 5].q].q;
+    o[Olive + 5].q += o[0].q;
+    o[0].q          = o[Olive + 5].q / o[Olive + 6].q;
+    o[Olive + 5].q  = o[Olive + 5].q - o[0].q * o[Olive + 6].q;
+    o[bti+1].q      = w;
+    o[bti-1].q      = r;
+    o[bti-2].q      = d;
+    m(a, bti, o, Yellow(=), White(=), s);
   } else
     m(a, w, o, r, d, s);
 }
@@ -47,40 +64,33 @@ N(tab) {
 }
 N(b_term) { if (Green(==) && White(==)) { if (s[0] == 'b') s++; else Blue(=); } m(a, w, o, r, d, s); }
 N(a_term) { if (Green(==) && White(==)) { if (s[0] == 'a') s++; else Blue(=); } m(a, w, o, r, d, s); }
-N(orand) {
-  if (White(==)) {
-    if      (Yellow (==)) Green (=), Black(=),  (o[w - 1].cs = s);
-    else if (Blue   (==)) Yellow(=),            (s = o[w - 1].cs);
-    else if (Green  (==)) Yellow(=),            (s = o[w - 1].cs);
-  } else if (Green  (==)) White (=);
-  m(a, w, o, r, d, s);
-}
 N(baaa) {
   if (Green(==) && Black(==))
     s = "baaa";
   m(a, w, o, r, d, s);
 }
-N(S);
-D(S0,   B(Green, T(b_term)))
-D(S1,   B(Green, T(S),    T(a_term)))
-D(S,    B(Green, T(S0),   T(orand), T(S1)))
-D(show, B(Green, T(baaa), T(S)))
+N(prints) { if(Green(==) && White(==)) printf("%s", o[w+1].cs); m(a,w,o,r,d,s); }
 D(E,                 //
-  B(Green, T(tina))  //
-  B(Lime,  T(kargi)) //
-  B(Olive, T(gogoa)) //
+  B(Lime,  Ta(prints, "{") )B(Green, Ta(prints, "}") ) //
+  B(Lime,  Ta(prints, "(") )B(Green, Ta(prints, ")") ) //
+);
+D(F,                 //
+  B(Lime,  Ta(prints, "(") )B(Green, Ta(prints, ")") ) //
+  B(Lime,  Ta(prints, "{") )B(Green, Ta(prints, "}") ) //
 );
 
 void text_index_init();
 int main() {
   long a = 0;
   long w = 0;
-  o_t o[2048];
+  o_t  o[2048];
   long r;
   long d;
   const char *s = "";
+  o[a++].q = 1;
   text_index_init();
-  (w = a + TW / 2), T(b), T(id), T(show), T(id), T(dot);
+  (w = a + TW / 2),
+                    T(b), Ta(prints,"s"), T(E), Ta(prints,"m"), T(F), T(id), Ta(prints,"e\n"), T(dot);
   m(a, w, o, Yellow(=), White(=), s);
   return 0;
 }
