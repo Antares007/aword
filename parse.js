b739e1ed191ee66dcdc7459f53f733644fb07359
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-06-18 07:26:59 +0400

    sync

--- parse.js
+++ parse.js
@@ -131,7 +131,7 @@ function add_missing_rays([n, b]) {
   };
   deleteDefinedRays("G");
   deleteDefinedRays("R");
-  for (let k in rays) b = b + `\nR(${rays[k]}) { ${rays[k]}(t, a, b, o, s); }`;
+  for (let k in rays) b = b + `\nR(${rays[k].padEnd(8,' ')}) { ${rays[k].padEnd(8,' ')}(t, a, b, o, s); }`;
   return [n, b];
 }
 async function compile([n, b]) {
--- root.c
+++ root.c
@@ -8,15 +8,21 @@ static N(T);
 void *map_file(const char *file_name);
 long load_file(void *memory, const char *aw_name);
 void monitor_init();
-
+N(Maroon_end) { P;}
+N(Olive_end ) { P;}
+N(Navy_end  ) { P;}
 int main(int argc, const char **argv) {
   long t = 0;
   long a = 0;
-  void *b = map_file("ram.ram");
-  void *o[512];
+  void*b = map_file("ram.ram");
+  void*o[512];
   long s = sizeof(o) / sizeof(*o);
 
   assert(b);
+
+  o[--s] = Maroon_end;
+  o[--s] = Olive_end;
+  o[--s] = Navy_end;
 
   o[a++] = printf;
   o[a++] = usleep;
@@ -24,19 +30,16 @@ int main(int argc, const char **argv) {
   o[a++] = (void *)1;
 
   o[a++] = "b";
-  o[a++] = "show";
-  o[a++] = "o";
-  o[a++] = (void *)3;
+  o[a++] = (void *)1;
   T(t, a, b, o, s);
 }
 static N(T) {
-  long count = (long)o[--a];
-  const char **asentence = (const char **)&o[a -= count];
+  long word_count = (long)o[--a];
+  const char **asentence = (const char **)&o[a -= word_count];
 
-  o[--s] = (void *)t;
   n_t aword = b + t;
   t += load_file(b + t, "tab");
-  for (long i = 0; i < count; i++)
+  for (long i = 0; i < word_count; i++)
     t += load_file(b + t, asentence[i]);
   t += load_file(b + t, "o");
   (aword + 16)(t, a, b, o, s);
