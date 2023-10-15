340f1725b0049b07d763c466a37736b72be5eab6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-10-15 07:58:08 +0400

    add DBT beauty

--- light.c
+++ light.c
@@ -4,156 +4,199 @@ typedef N((*n_t));
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
-#define W(nar) #nar, 0, nar, 0, 0, #nar, 0, ti, 0, 0
 const char *rays[] = {"Olive", "Maroon", "Lime", "Navy", 0, "Blue",  "Green",  "Red",  "Yellow"};
-#define P printf("%10s %10s\n", (char*)t[-2], rays[(r + 1) * d + 4]), usleep(200000)
-N(go ) {
+#define P printf("%10s %10s %10s\n", (char*)t[-2], rays[(r + 1) * d + 4], __FUNCTION__), usleep(200000)
+void ti(const char*n, long r, long d);
+N(go      ) { 
+  ti(t[-2], r, d);
   ((n_t)t[d * 5])(t + d * 5, a, b, o, s, r, d);
 }
-N(ti ) { go(t, a, b, o, s, r,  d); }
-N(fwd) { go(t, a, b, o, s, r, +1); }
-N(bo ) { 
+N(bo_heart) {
   if (r == 3 || r == 2) P;
-  else go(t, a, b, o, s, 3, 1);
+  else go(t, 0, b, o, t[1], 3, 1);
 }
-N(dot) { go(t, a, b, o, s, r, -1); }
-N(ln ) {
-  if (d == 1) {
-    for (long i = 0; i < a; i++)
-      printf("%s", (char*)o[i]);
-    (a = 0), printf("\n");
-  }
-  go(t, a, b, o, s, r, d);
+N(bo      ) {
+  t[0] = bo_heart;
+  t[1] = s;
+  bo_heart(t, a, b, o, s, r, d);
 }
-
-
-N(tab ) { ((n_t *)o[b])[r](o[b + 1], a, b + 2, o, s, r, d); }
+N(dot ) { go(t, a, b, o, s, r, -1); }
+N(tab ) { ti(t[-2], r, d); ((n_t *)o[b])[r](o[b + 1], a, b + 2, o, s, r, d); }
 typedef struct t_t {
-  long i;
-  long count;
+  long   i;
+  long   count;
   void **arms;
+  char  *fruitful;
 } t_t;
-N(connect_Green_Navy   ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(connect_Green_Lime   ) { go(t, a, b, o, s, 1, 1); }
-N(connect_Green_Maroon ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(connect_Green_Olive  ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(tw_Green             ) {
-  static n_t tab[] = {
-    connect_Green_Navy,
-    connect_Green_Lime,
-    connect_Green_Maroon,
-    connect_Green_Olive
-  };
-  t_t *c = t[+1]; (o[--b] = t), (o[--b] = tab), go(c->arms[c->i], a, b, o, s, r, d);
-}
-N(connect_Yellow_Navy  ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(connect_Yellow_Lime  ) { go(t, a, b, o, s, 1, 1); }
-N(connect_Yellow_Maroon) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(connect_Yellow_Olive ) {
+N(tab_switch) { ((n_t *)t[-1])[(r + 1) * d + 4](t, a, b, o, s, r, d); }
+#define Connector(Yellow)                               \
+  N(connect_##Yellow) {                                 \
+    static n_t tab[] = {                                \
+      connect_##Yellow##_Navy,                          \
+      connect_##Yellow##_Lime,                          \
+      connect_##Yellow##_Maroon,                        \
+      connect_##Yellow##_Olive                          \
+    };                                                  \
+    t_t *c = t[+1];                                     \
+    ti(t[-2], r, d), (o[--b] = t), (o[--b] = tab), \
+    go(c->arms[c->i], a, b, o, s, r, d);                \
+  }
+
+N(connect_Yellow_Navy  ) { go(t, a, b, o, s, r, d); }
+N(connect_Yellow_Lime  ) {
   t_t *c = t[+1];
+  c->fruitful[c->i] = 1;
+  go(t, a, b, o, s, r, +1);
+}
+N(connect_Yellow_Maroon) {
+  assert(d < 0 && r == 2);
+  t_t *c = t[+1];
+  char is_fruitful = c->fruitful[c->i];
+  c->i = (c->i + 1) % c->count;
+  if (is_fruitful) {
+    go(t, a, b, o, s, c->i == 0 ? 2 : 0, +1);
+  } else if (c->i == 0) {
+    if (1 < c->count) c->count--, go(t, a, b, o, s, r, +1);
+    else go(t, a, b, o, s, r, d);
+  } else {
+    c->count--;
+    c->i--;
+    for (long i      = c->i; i < c->count; i++)
+      (c->arms[i]    = c->arms[i + 1]),
+      (c->fruitful[i] = c->fruitful[i + 1]);
+    go(t, a, b, o, s, 0, d);
+  }
+}
+N(connect_Yellow_Olive) {
+  t_t *c = t[+1];
+  c->fruitful[c->i] = 1;
   c->i = (c->i + 1) % c->count;
   go(t, a, b, o, s, c->i == 0 ? 3 : 1, 1);
 }
-N(tw_Yellow) {
-  static n_t tab[] = {
-    connect_Yellow_Navy,
-    connect_Yellow_Lime,
-    connect_Yellow_Maroon,
-    connect_Yellow_Olive
-  };
+Connector(Yellow)
+N(connect_Red_Navy  ) { go(t, a, b, o, s, r, d); }
+N(connect_Red_Lime  ) { P; }
+N(connect_Red_Maroon) { 
   t_t *c = t[+1];
-  (o[--b] = t), (o[--b] = tab), go(c->arms[c->i], a, b, o, s, r, d);
+  c->i = (c->i + 1) % c->count;
+  go(t, a, b, o, s, c->i == 0 ? 2 : 0, 1);
 }
-N(full_heart) { ((n_t *)t[-1])[(r + 1) * d + 4](t, a, b, o, s, r, d); }
-N(Olive_stop  ) { go(t, a, b, o, s, r, d); }
-N(Maroon_stop ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(Lime_stop   ) { go(t, a, b, o, s, r, d); }
-N(Navy_stop   ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(Blue_stop   ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-N(Red_stop    ) { printf("%s %s\n", __FUNCTION__, rays[(r + 1) * d + 4]); }
-
-#define S(...) 2 + (void *[]){W(tab), __VA_ARGS__, W(dot)}
-#define T(name, ...)                                                        \
-  N(name) {                                                                 \
-    static n_t full_tab[] = {                                               \
-        Olive_stop,                                                         \
-        Maroon_stop,                                                        \
-        Lime_stop,                                                          \
-        Navy_stop,                                                          \
-        0,                                                                  \
-        Blue_stop,                                                          \
-        tw_Green,                                                           \
-        Red_stop,                                                           \
-        tw_Yellow,                                                          \
-    };                                                                      \
-    t[-1] = full_tab;                                                       \
-    t[+0] = full_heart;                                                       \
-    t[+1] = &(t_t){ .i = 0,                                                 \
-                    .count = sizeof((void *[]){__VA_ARGS__})/sizeof(void*), \
-                    .arms = (void *[]){__VA_ARGS__}};                       \
-    full_heart(t, a, b, o, s, r, d);                                          \
-  }
-
-N(n1  ) { if (d == 1) o[a++] = "1"; go(t, a, b, o, s, r, d); }
-N(n2  ) { if (d == 1) o[a++] = "2"; go(t, a, b, o, s, r, d); }
-N(n3  ) { if (d == 1) o[a++] = "3"; go(t, a, b, o, s, r, d); }
-N(n4  ) { if (d == 1) o[a++] = "4"; go(t, a, b, o, s, r, d); }
-N(n5  ) { if (d == 1) o[a++] = "5"; go(t, a, b, o, s, r, d); }
-
-N(Yellow_term) {
-  if (*s == *(char*)t[1]) (o[a++] = (void*)(long)*s), go(t, a, b, o, s + 1, r, d); // Yellow
-  else go(t, a, b, o, s, 2, -1); // Maroon
+N(connect_Red_Olive ) { P; }
+Connector(Red)
+N(connect_Green_Navy   ) { go(t, a, b, o, s, r, d); }
+N(connect_Green_Lime   ) {
+  t_t *c = t[+1];
+  c->fruitful[c->i] = 1;
+  go(t, a, b, o, s, r, +1);
 }
-N(Green_term) { 
-  if (*s == *(char*)t[1]) (o[a++] = (void*)(long)*s), go(t, a, b, o, s + 1, r, d); // Green
-  else go(t, a, b, o, s, 0, -1); // Navy
+N(connect_Green_Maroon ) { P; }
+N(connect_Green_Olive  ) { P; }
+Connector(Green)
+N(connect_Blue_Navy   ) { go(t, a, b, o, s, r, d); }
+N(connect_Blue_Lime   ) { P; }
+N(connect_Blue_Maroon ) { P; }
+N(connect_Blue_Olive  ) { P; }
+Connector(Blue)
+N(t_heart) {
+  static n_t full_tab[] = {
+      go, go, go, go,
+      0,
+      connect_Blue,
+      connect_Green,
+      connect_Red,
+      connect_Yellow,
+  };
+  t[-1]  = full_tab;
+  t[+0]  = tab_switch;
+  t_t *c = t[+1];
+  char fruitful[c->count];
+  for (long i = 0; i < c->count; i++) fruitful[i] = 0;
+  c->fruitful = fruitful;
+  if (r == 2) go(t, a, b, o, s, 0, -1);
+  else tab_switch(t, a, b, o, s, r, d);
+}
+
+N(n0  ) { if (d == 1) o[a++] = (void*)'0'; go(t, a, b, o, s, r, d); }
+N(n1  ) { if (d == 1) o[a++] = (void*)'1'; go(t, a, b, o, s, r, d); }
+N(n2  ) { if (d == 1) o[a++] = (void*)'2'; go(t, a, b, o, s, r, d); }
+N(n3  ) { if (d == 1) o[a++] = (void*)'3'; go(t, a, b, o, s, r, d); }
+N(n4  ) { if (d == 1) o[a++] = (void*)'4'; go(t, a, b, o, s, r, d); }
+N(n5  ) { if (d == 1) o[a++] = (void*)'5'; go(t, a, b, o, s, r, d); }
+
+N(Yellow_Green_term) {
+  if (*s == *(char*)t[1])
+    (o[a++] = (void*)(long)*s),
+      go(t, a, b, o, s + 1, r, d);
+  else go(t, a, b, o, s, r - 1, -1);
 }
 N(term  ) { 
   static n_t tab[] = {
-    go,           // Olive
-    go,           // Maroon
-    go,           // Lime
-    go,           // Navy
-    go,           // 0
-    go,           // Blue
-    Green_term,   // Green                                                          
-    go,           // Red
-    Yellow_term,  // Yellow                                                          
+    go,                 // Olive
+    go,                 // Maroon
+    go,                 // Lime
+    go,                 // Navy
+    go,                 // 0
+    go,                 // Blue
+    Yellow_Green_term,  // Green                                                          
+    go,                 // Red
+    Yellow_Green_term,  // Yellow                                                          
   }; 
   t[-1] = tab;
-  t[ 0] = full_heart;
-  full_heart(t, a, b, o, s, r, d);
+  t[ 0] = tab_switch;
+  tab_switch(t, a, b, o, s, r, d);
 };
-N(at  ) { t[0] = term; t[+1] = "a"; term(t, a, b, o, s, r, d); }
-N(bt  ) { t[0] = term; t[+1] = "b"; term(t, a, b, o, s, r, d); }
+N(at  ) { t[+1] = "a"; term(t, a, b, o, s, r, d); }
+N(bt  ) { t[+1] = "b"; term(t, a, b, o, s, r, d); }
+N(st  ) { t[+1] = "s"; term(t, a, b, o, s, r, d); }
 N(p_chars ) {
   if (d == 1) {
+    printf("%s(", rays[(r + 1) * d + 4]);
     for (long i = 0; i < a; i++)
       printf("%c", (char)(long)o[i]);
-    (a = 0), printf("\n");
+    (a = 0), printf(")\n");
   }
   go(t, a, b, o, s, r, d);
 }
+#define T(nar) #nar, 0, nar, 0, 0
+#define B(...) (2 + (void*[]) {T(tab), __VA_ARGS__, T(dot)})
+#define D(name, ...)                                        \
+  N(name) {                                                 \
+    void*arms[] = { __VA_ARGS__ };                          \
+    t[1] = &(t_t) {                                         \
+      .i = 0,                                               \
+      .count = sizeof(arms) / sizeof(*arms),                \
+      .arms = arms };                                       \
+    t_heart(t, a, b, o, s, r, d);                           \
+  }
 
-T(n345,
-  S(W(n3)),
-  S(W(n4)),
-  S(W(n5))) 
-T(n123,
-  S(W(n1)),
-  S(W(n2)),
-  S(W(n3), W(n345))) 
-
+D(n345,
+  B(T(n3)),
+  B(T(n4)),
+  B(T(n5)))
+D(n123,
+  B(T(n1)),
+  B(T(n2)),
+  B(T(n3)))
+N(ε) { go(t, a, b, o, s, r, d); }
+D(sS,
+  B(T(n0), T(ε)),
+  B(T(n1), T(st), T(sS), T(sS)),
+  B(T(n2), T(st), T(sS), T(sS)))
+D(εab,
+  B(T(ε)),
+  B(T(at)),
+  B(T(bt)))
+D(abS, B(T(εab), T(εab), T(εab)))
 void ti_init();
+void ti_step(const char*, long, long);
+void ti(const char*n, long r, long d) {
+  //ti_step(n, r, d);
+}
 int main() {
-  void **text = 2 + (void *[]){W(bo),
-    W(at),
-    W(bt),
-    //W(n123), W(n123), W(n123),
-    W(p_chars),
-    W(dot)};
-  long a = 0;
+  void **text = 2 + (void *[]){ T(bo), T(sS), T(p_chars), T(dot) };
+  long  a = 0;
   void *o[512];
-  long b = sizeof(o) / sizeof(*o);
-  go(text, a, b, o, "ab", 3, 1);
+  long  b = sizeof(o) / sizeof(*o);
+  ti_init();
+  go(text, a, b, o, "ss", 3, 1);
 }
--- light_text_index.c
+++ light_text_index.c
@@ -43,11 +43,15 @@ void draw(step_t *steps, long count) {
 
     Vector2 zero = {2 * GetScreenWidth() / 8.f, 3 * GetScreenHeight() / 8.f};
     Vector2 dir = {0, -1};
-    for (long i = 0; i < count; i++) {
+    long si = 0;
+    for (long i = si; i < count; i++) {
       step_t *s = &steps[i];
+      if (strcmp(s->name, "bo") == 0) {
+        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
+                      (Color){.a = 150, .r = -1, .g = -1, .b = -1});
+        si = i;
+      }
       Vector2 ns = {90, 50};
-      float step_size = ((long)dir.x ? ns.x : ns.y) * s->d;
-      zero = Vector2Add(zero, Vector2Scale(dir, zoom * step_size));
       Camera2D camera = {.target = {0, 0},
                          .rotation = 0,
                          .zoom = zoom,
@@ -59,10 +63,12 @@ void draw(step_t *steps, long count) {
       DrawRectangleLines(-ns.x / 2, -ns.y / 2, ns.x, ns.y, BLACK);
       DrawTextEx(font, s->name, (Vector2){-ns.x / 2, -ns.y / 2}, 35, 1, fgcolor);
       EndMode2D();
-      //if (i < count-1 && strcmp(steps[i + 1].name, "tab") == 0 && s->d == 1) 
-      //  dir = Vector2Rotate(dir, M_PI_2 * s->d);
-      //if (strcmp(steps[i].name, "tab") == 0 && s->d == -1) 
-      //  dir = Vector2Rotate(dir, M_PI_2 * s->d);
+      if (i < count-1 && strcmp(steps[i + 1].name, "tab") == 0 && s->d == 1) 
+        dir = Vector2Rotate(dir, M_PI_2 * s->d);
+      float step_size = ((long)dir.x ? ns.x : ns.y) * s->d;
+      zero = Vector2Add(zero, Vector2Scale(dir, zoom * step_size));
+      if (strcmp(steps[i].name, "tab") == 0 && s->d == -1) 
+        dir = Vector2Rotate(dir, M_PI_2 * s->d);
     }
     EndDrawing();
     key = GetCharPressed();
