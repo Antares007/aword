d28aa46e810d187d748a389d4adeb85e2d38e716
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-12-20 14:57:27 +0400

    new twist

--- light77.c
+++ light77.c
@@ -1,197 +1,166 @@
-#define P                                                                      \
-  printf("%4ld %7s %s\n", τ, rays[(ρ + 1) * δ + 5], __FUNCTION__), usleep(200000)
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #define N(argo)                                                                \
-  void argo(void **ο, long β, long α, long τ, long σ, long ρ, long δ)
+  void argo(long*ο, long β, long α, long τ, long σ, long ρ, long δ)
 typedef N((*n_t));
 
 const char *rays[] = {"Fuchsia", "Olive", "Maroon", "Lime",   "Navy",  0,
                       "Blue",    "Green", "Red",    "Yellow", "Purple"};
-
-#define T(nar) (ο[-σ] = ο[+σ] = nar), (σ += 11), (ο[-σ] = ο[+σ] = 0)
+#define P printf("%4ld %4ld %4ld %4ld %7s %s\n", σ, α, β, (τ/Σ+1)*δ, rays[(ρ + 1) * δ + 5], __FUNCTION__), usleep(200000)
 
 #define oba ο, β, α
 #define obatsrd ο, β, α, τ, σ, ρ, δ
 #define srd σ, ρ, δ
-
-N(goTo      ) { ((n_t)ο[τ + 11 * δ])(oba, τ + 11 * δ, srd); }
+#define Σ 2048
+N(goTo      ) { ((n_t)ο[τ + Σ*ρ + 11*δ])(oba, τ + 11*δ, srd); }
 N(o         );// { goTo(oba, τ, σ, ρ, -δ); }
+N(δ_switch  ) { ((n_t)ο[τ + Σ*ρ + δ])(obatsrd); }
 
-N(δ_switch  ) { ((n_t)ο[τ + δ])(obatsrd); }
-N(ρδ_switch ) { ((n_t)ο[τ + (ρ + 1) * δ])(obatsrd); }
-
-#define OB ((unsigned long*)&ο[(long)ο[β]])
-#define Arms     OB[-5]
-#define I        OB[+5]
-#define Fruitful ο[OB[I]+5]
-#define Trimed   ο[OB[I]+4]
-N(propeller) {
-  long oi = I;
-  while((I = (I + 1) % Arms) && ο[OB[I]+5]);
-  long d = (long)ο[--α];
-  goTo(oba,τ,σ, !(oi < I) * 2 + d, δ);
+#define Blue(τ)   (τ)
+#define Green(τ)  (Σ+Blue(τ))
+#define Red(τ)    (Σ+Σ+Blue(τ))
+#define Yellow(τ) (Σ+Σ+Σ+Blue(τ))
+N(term_b) {}
+N(term_a) { printf("%20s %7s\n", (char*)ο[τ+5], rays[(ρ + 1) * δ + 5]), usleep(200000), goTo(obatsrd); }
+N(term) {
+  ο[Yellow(σ)] = term_a, ο[Red(σ)] = term_a, ο[Green(σ)] = term_a, ο[Blue(σ)  ] = term_a, 
+                                                                   ο[Blue(σ)+5] = ο[--α],
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α-1, τ, srd);
 }
-N(Red_Blue_propeller    ) { (ο[α++] = (void*)0),propeller(obatsrd); }
-N(Yellow_Green_propeller) { (ο[α++] = (void*)1),propeller(obatsrd); }
-N(Yellow_Green          ) { Fruitful = 1,       goTo(obatsrd); }
-N(Yellow_Red            ) { if (Arms == 1)      goTo(obatsrd);
-                            else if (Fruitful)  Red_Blue_propeller(obatsrd);
-                            else Trimed = 1,    Red_Blue_propeller(obatsrd); }
-N(Yellow_Yellow         ) { Fruitful = 1,       Yellow_Green_propeller(obatsrd); }
-N(Red_Red               ) {                     Red_Blue_propeller(obatsrd); }
-N(Green_Green           ) { Fruitful = 1,       goTo(obatsrd); }
-N(nop) {assert(0);}
-N(Blue) {
-  ο[τ-4*δ] = goTo;
-  ο[τ-3*δ] = goTo;
-  ο[τ-2*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  ο[τ    ] = ρδ_switch;
-  ο[τ+1*δ] = goTo;
-  ο[τ+2*δ] = nop;
-  ο[τ+3*δ] = nop;
-  ο[τ+4*δ] = nop;
+// .....S....
+//      S[5] = G A D  ttt  A ⚙ G  ttt  A ⚙
+//      t actionable text
+//      G goTo_t5
+//      A ward_arm
+//      D door
+//      1 propeller1
+//      2 propeller2
+//      
+N(goTo_t5) { goTo(oba, ο[τ+5], srd); }
+N(tab) { 
+  ο[Yellow(σ)  ] = ο[Red(σ)  ] = ο[Green(σ)  ] = ο[Blue(σ)  ] = goTo_t5;
+                                                 ο[Blue(σ)+5] = ο[β-5];
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(goTo_ward) { goTo(oba, ο[τ-5], σ, ο[τ-4], δ); }
+N(ward_arm_nar) { (ο[ο[τ+5] - 5] = τ), (ο[ο[τ+5] - 4] = ρ), goTo(obatsrd); }
+N(ward_arm) { 
+  ο[Yellow(σ)  ] = ο[Red(σ)  ] = ο[Green(σ)  ] = ο[Blue(σ)  ] = ward_arm_nar;
+                                                 ο[Blue(σ)+5] = ο[β-5];
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(close_door) { (ο[Red(τ)] = ο[Blue(τ)] = δ_switch), goTo(obatsrd); }
+N(open_door ) { (ο[Red(τ)] = ο[Blue(τ)] = goTo), goTo(obatsrd); }
+N(door) { 
+  ο[Yellow(σ)-1] = close_door;ο[Red(σ)-1] = goTo;                        ο[Blue(σ)-1] = goTo;
+  ο[Yellow(σ)  ] = δ_switch;  ο[Red(σ)  ] = δ_switch;ο[Green(σ)] = goTo; ο[Blue(σ)  ] = δ_switch;
+  ο[Yellow(σ)+1] = open_door; ο[Red(σ)+1] = goTo_t5;                     ο[Blue(σ)+1] = goTo_t5;
+                                                                         ο[Blue(σ)+5] = ο[β-5];
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(next_arm) { ο[ο[τ+5]+5] = τ+11, goTo_t5(oba,τ,σ,ρ-2,δ); }
+N(propeller1) {
+  ο[Yellow(σ)] = next_arm,  ο[Red(σ)] = next_arm,  ο[Green(σ)] = goTo_t5, ο[Blue(σ)] = goTo_t5,
+                                                                          ο[Blue(σ)+5] = ο[β-5],
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(reset_arm) { ο[ο[τ+5]+5] = ο[ο[τ+5]+4], goTo_t5(obatsrd); }
+N(propeller2) {
+  ο[Yellow(σ)] = reset_arm, ο[Red(σ)] = reset_arm, ο[Green(σ)] = goTo_t5, ο[Blue(σ)] = goTo_t5,
+                                                                          ο[Blue(σ)+5] = ο[β-5],
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(o_next) { 
+  ο[α++] = ward_arm;
+  ο[α++] = propeller1;
+  ο[α++] = tab;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(o_last) { 
+  ο[α++] = ward_arm;
+  ο[α++] = propeller2;
+  ο[β-4] = o_next;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
+}
+N(o) { ((n_t)ο[β-4])(obatsrd); }
+N(var_end) { 
+  β -= 5;
+  ο[Yellow(τ)+1] = goTo_t5,ο[Red(τ)+1] = goTo_t5, ο[Green(τ)+1] = goTo_t5,ο[Blue(τ)+1] = goTo_t5,     
+  ο[τ+4] = ο[τ+5] = σ + 11;
   ((n_t)ο[τ])(obatsrd);
 }
-N(Green) {
-  ο[τ-4*δ] = goTo;
-  ο[τ-3*δ] = goTo;
-  ο[τ-2*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  ο[τ    ] = ρδ_switch;
-  ο[τ+1*δ] = goTo;
-  ο[τ+2*δ] = Green_Green;
-  ο[τ+3*δ] = nop;
-  ο[τ+4*δ] = nop;
-  ((n_t)ο[τ])(obatsrd);
+N(var);
+N(var_start) { 
+  ο[β++] = τ; ο[β++] = o_last; ο[β++] = var; ο[β++] = term; ο[β++] = o;
+  ο[α++] = var_end; ο[α++] = tab; ο[α++] = ward_arm; ο[α++] = door;
+  ((n_t)ο[Blue(τ+5)])(obatsrd);
 }
-N(Red) {
-  ο[τ-4*δ] = goTo;
-  ο[τ-3*δ] = goTo;
-  ο[τ-2*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  ο[τ    ] = ρδ_switch;
-  ο[τ+1*δ] = goTo;
-  ο[τ+2*δ] = nop;
-  ο[τ+3*δ] = Red_Red;
-  ο[τ+4*δ] = nop;
-  ((n_t)ο[τ])(obatsrd);
+N(var) { 
+  ο[Yellow(σ)-1] = goTo_ward,ο[Red(σ)-1] = goTo_ward, ο[Green(σ)-1] = goTo_ward,ο[Blue(σ)-1] = goTo_ward,
+  ο[Yellow(σ)  ] = δ_switch, ο[Red(σ)  ] = δ_switch,  ο[Green(σ)  ] = δ_switch, ο[Blue(σ)  ] = δ_switch,
+  ο[Yellow(σ)+1] = var_start,ο[Red(σ)+1] = var_start, ο[Green(σ)+1] = var_start,ο[Blue(σ)+1] = var_start,
+                                                                                ο[Blue(σ)+5] = ο[--α],
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
 }
-N(Yellow) {
-  ο[τ-4*δ] = goTo;
-  ο[τ-3*δ] = goTo;
-  ο[τ-2*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  ο[τ    ] = ρδ_switch;
-  ο[τ+1*δ] = goTo;
-  ο[τ+2*δ] = Yellow_Green;
-  ο[τ+3*δ] = Yellow_Red;
-  ο[τ+4*δ] = Yellow_Yellow;
-  ((n_t)ο[τ])(obatsrd);
+N(S) { 
+  n_t o = ο[β-1], term = ο[β-2], var = ο[β-3]; (void)var;
+  ο[α++] = "b", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "a", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "t", ο[α++] = term, ο[α++] = o, ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
 }
-N(door_open) {
-  ο[τ-4*δ] = goTo;
-  ο[τ-3*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  goTo(obatsrd); 
+N(n345) { 
+  n_t o = ο[β-1], term = ο[β-2], var = ο[β-3]; (void)var;
+  ο[α++] = "3", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "4", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "5", ο[α++] = term, ο[α++] = o, ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
 }
-N(door_close) {
-  ο[τ-4*δ] = door_open;
-  ο[τ-3*δ] = o;
-  ο[τ-1*δ] = o;
-  goTo(obatsrd); 
+N(n123) { 
+  n_t o = ο[β-1], term = ο[β-2], var = ο[β-3]; (void)var;
+  ο[α++] = "1", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "2", ο[α++] = term, ο[α++] = o,
+  ο[α++] = "3", ο[α++] = term, ο[α++] = n345, ο[α++] = var, ο[α++] = o, ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
 }
-N(door) {
-  ο[τ-4*δ] = door_close;
-  ο[τ-3*δ] = goTo;
-  ο[τ-2*δ] = goTo;
-  ο[τ-1*δ] = goTo;
-  ο[τ] = ρδ_switch;
-  ο[τ+1*δ] = o;
-  ο[τ+2*δ] = goTo;
-  ο[τ+3*δ] = o;
-  ο[τ+4*δ] = door_open;
-  ((n_t)ο[τ])(obatsrd); 
-}
-N(b         ) {P; goTo(obatsrd); }
-N(a         ) {P; goTo(obatsrd); }
-N(t         ) {P; goTo(obatsrd); }
-N(s         ) {P; goTo(obatsrd); }
-N(id1       ) {P; goTo(obatsrd); }
-N(id2       ) {P; goTo(obatsrd); }
-N(id3       ) {P; goTo(obatsrd); }
-N(id4       ) {P; goTo(obatsrd); }
-N(id5       ) {P; goTo(obatsrd); }
-N(o_tab   ) { goTo(ο, β+1, α, ο[β], srd); }
-N(o_b     ) { goTo(oba, τ, σ, ρ, -δ); }
-N(o) {
-  long st = ο[α-2];
-  long ti = ο[α-3];
-  assert(0 < ti);
-  ο[-st+ti] = ο[+st+ti] = τ;
-  ο[α-3] = (long)ο[α-3] - 1;
 
-  ο[-τ+1] = ο[+τ-1] = o_tab;
-  ο[-τ-1] = ο[+τ+1] = o_b;
-  ο[-τ  ] = ο[+τ  ] = δ_switch;
-  goTo(obatsrd);
-}
-N(var_toti) {
-  ο[--β] = τ;
-  ο[--β] = (long)ο[ρ+1] * -δ;
-  goTo(oba, (long)ο[τ+(long)ο[τ+5]] * δ, srd);
-}
-N(var) {
-  long sd = ο[--α];
-  long st = ο[--α];
-  long ti = ο[--α];
-  assert(1 < ti);
-  ο[-st+1 ] = ο[+st-1 ] = goTo;
-  ο[-st-1 ] = ο[+st+1 ] = var_toti;
-  ο[-st   ] = ο[+st   ] = δ_switch;
-  ο[-st+ti] = ο[+st+ti] = τ;
-  ο[-st+5 ] = ο[+st+5 ] = ti;
-  ο[-st-5 ] = ο[+st-5 ] = 5 - ti;
+N(b) { goTo(obatsrd); }
+N(a) { goTo(obatsrd); }
+N(t) { goTo(obatsrd); }
 
-  ο[-τ] = ο[τ] = o_tab;
-  goTo(oba, st, σ, ρ, sd);
+N(dot_turn) { P; ο[β++] = ρ; goTo(oba, τ, σ, ρ, -δ); }
+N(dot  ) {
+  ο[Yellow(σ)] = dot_turn, ο[Red(σ)] = dot_turn,  ο[Green(σ)] = dot_turn, ο[Blue(σ)] = dot_turn,
+  σ -= 11;
+  ((n_t)ο[α-1])(ο, β, α - 1, τ, srd);
 }
-N(bark) {
-  ο[α++] = 5;
-  ο[α++] = τ;
-  ο[α++] = δ;
-  goTo(oba, σ, σ, ρ, -1);
-}
-N(S) {
-  T(var), T(b), T(o),
-          T(a), T(o),
-          T(t), T(o), bark(obatsrd);
-}
-N(origin_pith) { P; if (ρ < 2) goTo(oba, τ, σ, 3, δ); }
-N(origin) {
-  long sd = ο[--α];
-  long st = ο[--α];
-  long i = ο[--α];
-  assert(!i);
-  ο[τ] = origin_pith;
-  goTo(ο, β, α, st, σ, ρ, sd);
+N(nop  ) { assert(0); }
+N(aword) { P; long p = ο[--β]; if(p < 2) goTo(oba, τ, σ, ρ, -δ); }
+N(tword) {
+  ο[Yellow(σ)] = aword, ο[Red(σ)] = nop, ο[Green(σ)] = nop, ο[Blue(σ)] = nop,
+  goTo(oba, σ, σ-11, ρ, δ);
 }
 int main() {
-  const long size = 512;
-  void*ram[size + 1 + size];
-  void**ο = ram + size;
-  long  β = +size; 
-  long  α = -size;
-  long  τ = 0;
-  long  σ = 0;
-  long  ρ = 3;
-  long  δ = 1;
-  T(origin),  T(S       ), T(o),
-              T(id1), T(o),
-              T(id2), T(o),
-              T(id3), T(o),
-              T(id4), T(o), bark(obatsrd);
+  long ο[Σ + Σ + Σ + Σ];
+  long β = Σ; 
+  long α = 0;
+  long τ = 0;
+  long σ = Σ;
+  long ρ = 3;
+  long δ = 1;
+  *ο = Σ;
+  ο[α++] = tword,
+  ο[α++] = "before",ο[α++] = term,
+  ο[α++] = n123,       ο[α++] = var,
+  ο[α++] = n123,       ο[α++] = var,
+  ο[α++] = n123,       ο[α++] = var,
+  ο[α++] = "after", ο[α++] = term,
+  ο[α++] = dot;
+  ((n_t)ο[α-1])(ο, β, α-1, τ, srd);
 }
