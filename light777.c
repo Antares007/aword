c3102e21e74805aff8989e94b35b9659efffe325
Archil Bolkvadze <a.bolkvadze@gmail.com>
2024-02-08 10:26:56 +0400

    cycle

--- light777.c
+++ light777.c
@@ -1,12 +1,13 @@
+// clang-format off
 #define N(argo)                                                                \
   void argo(long *ο, long β, long α, long τ, long σ, long ρ, long δ)
 typedef N((*n_t));
 
 #ifndef NDEBUG
 #define P                                                                      \
-  printf("%4ld %2ld %2ld %7s %s\n", α, ρ, δ, rays[(ρ + 1) * δ + 5],            \
+  printf("%4ld %4ld %4ld %7s %s\n", α, β, τ, rays[(ρ + 1) * δ + 5],            \
          __FUNCTION__),                                                        \
-      usleep(200000)
+      usleep(20000)
 #else
 #define P (void)0
 #endif
@@ -16,30 +17,110 @@ typedef N((*n_t));
 #include <unistd.h>
 const char *rays[] = {"Fuchsia", "Olive", "Maroon", "Lime",   "Navy",  0,
                       "Blue",    "Green", "Red",    "Yellow", "Purple"};
-#define oba ο, β, α
+#define oba     ο, β, α
 #define obatsrd ο, β, α, τ, σ, ρ, δ
-#define srd σ, ρ, δ
+#define     srd             σ, ρ, δ
 
-N(goto_515) { ((n_t)ο[τ + δ * 16])(ο, β, α, τ + δ * 11, σ, ρ, δ); }
-N(goto_a) { ((n_t)ο[α - 1])(ο, β, α - 1, τ, σ, ρ, δ); }
+N(goto_515  ) { ((n_t)ο[τ + δ * 16])(ο, β, α, τ + δ * 11, σ, ρ, δ); }
+N(goto_a    ) { ((n_t)ο[α - 1])(ο, β, α - 1, τ, σ, ρ, δ); }
 
-N(reverse_δ) { goto_515(oba, τ, σ, ρ, -δ); }
-N(cycle) { ρ = (ρ + 1) % 4, reverse_δ(obatsrd); }
-N(begin) { ο[σ - 5] = cycle, goto_515(oba, σ, σ - 11, 3, 1); }
-N(end) { ο[σ + 5] = reverse_δ, σ -= 11, goto_a(obatsrd); }
+N(reverse_δ ) { P, ο[--β] = ρ, goto_515(oba, τ, σ, ρ, -δ); }
+N(cycle     ) { if(ο[β++] < 2) P, goto_515(oba, τ, σ, 3, -δ); }
+N(begin     ) { ο[σ - 5] = cycle, goto_515(oba, σ, σ - 11, 3, 1); }
+N(end       ) { ο[σ + 5] = reverse_δ, σ -= 11, goto_a(obatsrd); }
 
-N(put_it) {
+N(term_bk) {
   P;
-  ο[α++] = ο[τ];
   goto_515(obatsrd);
 }
-N(put) {
+N(term_it) {
   P;
-  ο[σ - 5] = goto_515;
+  ο[α++] = ο[τ], goto_515(obatsrd);
+}
+N(term) {
+  ο[σ - 5] = term_bk;
   ο[σ] = ο[--α];
-  ο[σ + 5] = put_it;
+  ο[σ + 5] = term_it;
   σ -= 11, goto_a(obatsrd);
 }
+
+N(ρ_switch          ) { P; ((n_t)ο[τ + (ρ + 1) * δ])(obatsrd); }
+
+// G w w w S w w w O
+// G      WSG     WRG
+N(shift_it) { P, ο[ο[τ]] = τ + 11, goto_515(oba, τ, σ, ρ - 2, δ); }
+N(shift   ) {
+  ο[σ - 5] = goto_515,
+  ο[σ] = τ,
+  ο[σ + 1] = ο[σ + 2] = goto_515, ο[σ + 3] = ο[σ + 4] = shift_it;
+  ο[σ + 5] = ρ_switch;
+  σ -= 11, goto_a(obatsrd);
+}
+
+N(goto_it ) { P, goto_515(oba, ο[τ], srd); }
+N(goTo    ) { ο[σ - 5] = goto_it,  ο[σ] = τ, ο[σ + 5] = goto_it; σ -= 11, goto_a(obatsrd); }
+
+N(ward_it ) { P, ο[ο[τ] - 1] = τ, ο[ο[τ] - 2] = ρ, goto_515(obatsrd); }
+N(ward    ) { ο[σ - 5] = goto_515, ο[σ] = τ, ο[σ + 5] = ward_it; σ -= 11, goto_a(obatsrd); }
+
+N(reset_it) { P, ο[ο[τ]] = ο[ο[τ] + 1], goto_515(obatsrd); }
+N(reset   ) {
+  ο[σ - 5] = goto_515;
+  ο[σ] = τ;
+  ο[σ + 1] = ο[σ + 2] = goto_515, ο[σ + 3] = ο[σ + 4] = reset_it, ο[σ + 5] = ρ_switch;
+  σ -= 11, goto_a(obatsrd);
+}
+
+N(semicoma) {
+  ο[α++] = ward;
+  ο[α++] = shift;
+  ο[α++] = goTo;
+  goto_a(obatsrd);
+}
+N(dot     ) {
+  ο[α++] = ward;
+  ο[α++] = reset;
+  ο[α++] = goTo;
+  goto_a(obatsrd);
+}
+
+N(end_tran) {
+  ο[τ] = ο[τ + 1] = σ + 11, ο[τ + 5] = goto_it, goto_it(obatsrd);
+}
+N(run_tran) {
+  n_t transcript = ο[τ];
+  ο[α++] = end_tran;
+  ο[α++] = goTo;
+  transcript(obatsrd);
+}
+N(goto_ward) { P; goto_515(oba, ο[τ - 1], σ, ο[τ - 2], δ); }
+N(var) {
+  n_t transcript = ο[--α];
+  ο[σ - 5] = goto_ward, ο[σ] = transcript, ο[σ + 5] = run_tran, σ -= 11, goto_a(obatsrd);
+}
+
+N(id_Olive  ) { P, goto_515(obatsrd); }
+N(id_Maroon ) { P, goto_515(obatsrd); }
+N(id_Lime   ) { P, goto_515(obatsrd); }
+N(id_Navy   ) { P, goto_515(obatsrd); }
+N(id_Blue   ) { P, goto_515(obatsrd); }
+N(id_Green  ) { P, goto_515(obatsrd); }
+N(id_Red    ) { P, goto_515(obatsrd); }
+N(id_Yellow ) { P, goto_515(obatsrd); }
+N(id) {
+  P;
+  ο[σ - 5] = ρ_switch;
+  ο[σ + 1] = id_Blue;   ο[σ - 1] = id_Navy;
+  ο[σ + 2] = id_Green;  ο[σ - 2] = id_Lime;
+  ο[σ + 3] = id_Red;    ο[σ - 3] = id_Maroon;
+  ο[σ + 4] = id_Yellow; ο[σ - 4] = id_Olive;
+  ο[σ + 5] = ρ_switch;
+  σ -= 11, goto_a(obatsrd);
+}
+N(S) { P; }
+
+N(put_it  ) { P, ο[α++] = ο[τ], goto_515(obatsrd); }
+N(put     ) { ο[σ - 5] = goto_515, ο[σ] = ο[--α], ο[σ + 5] = put_it, σ -= 11, goto_a(obatsrd); }
 
 N(print_it) {
   P;
@@ -52,143 +133,23 @@ N(print_it) {
   }
   goto_515(obatsrd);
 }
-N(print) {
-  P;
-  ο[σ - 5] = goto_515;
-  ο[σ + 5] = print_it;
-  σ -= 11, goto_a(obatsrd);
-}
-//
-//   (((₀)₁)₁)₂
-//      0 1 1 2
-//  ((((₀)₁)₁)¹)₂
-//
-//      0 11  10
-//      ₀(₁₁ )₁₀
-//      ₀(₁₁₁)¹₀
-//      0 111 0
-//
-//      0 11 0
-//      ₀(₁₁)₀
-//      ₀(₁ )⁰
-//      0 1  10
-//
-N(term_it) {
-  P;
-  goto_515(obatsrd);
-}
-N(term) {
-  P;
-  ο[σ - 5] = goto_515;
-  ο[σ] = ο[--α];
-  ο[σ + 5] = term_it;
-  σ -= 11, goto_a(obatsrd);
-}
-
-N(semicoma_it) {
-  P;
-  ο[ο[τ]] = 0;
-}
-N(ρ_switch) { ((n_t)ο[τ + (ρ + 1) * δ])(obatsrd); }
-N(semicoma) {
-  P;
-  ο[σ - 5] = goto_515;
-  ο[σ] = ο[β];
-  ο[σ + 1] = semicoma_it;
-  ο[σ + 2] = semicoma_it;
-  ο[σ + 3] = semicoma_it;
-  ο[σ + 4] = semicoma_it;
-  ο[σ + 5] = ρ_switch;
-  σ -= 11, goto_a(obatsrd);
-}
-
-N(dot_it) {
-  P;
-  long v = ο[τ];
-  ο[v] = 0;
-}
-N(dot) {
-  P;
-  ο[σ - 5] = goto_515;
-  ο[σ] = τ;
-  ο[σ + 1] = dot_it;
-  ο[σ + 2] = dot_it;
-  ο[σ + 3] = dot_it;
-  ο[σ + 4] = dot_it;
-  ο[σ + 5] = ρ_switch;
-  σ -= 11, goto_a(obatsrd);
-}
-N(var_it) { P; }
-N(finish_typing_var) {
-  P;
-  ο[σ] = 0;
-  ο[τ] = σ;
-  ο[τ + 5] = var_it;
-  σ += 11, goto_515(obatsrd);
-}
-N(start_typing_var) {
-  P;
-  ο[α++] = finish_typing_var;
-  ((n_t)ο[τ])(obatsrd);
-}
-N(var) {
-  P;
-  ο[σ - 5] = goto_515;
-  ο[σ] = ο[--α];
-  ο[σ + 5] = start_typing_var;
-  σ -= 11, goto_a(obatsrd);
-}
-
+N(print) { ο[σ - 5] = goto_515, ο[σ + 5] = print_it, σ -= 11, goto_a(obatsrd); }
 N(bat) {
-  P;
-  ο[α++] = "b";
-  ο[α++] = term;
-  ο[α++] = semicoma;
-  ο[α++] = "a";
-  ο[α++] = term;
-  ο[α++] = semicoma;
-  ο[α++] = "t";
-  ο[α++] = term;
-  ο[α++] = dot;
-  goto_a(obatsrd);
+  ο[α++] = "b"; ο[α++] = term; ο[α++] = semicoma;
+  ο[α++] = "a"; ο[α++] = term; ο[α++] = semicoma;
+  ο[α++] = "t"; ο[α++] = term; ο[α++] = dot, goto_a(obatsrd);
 }
-N(id_Olive) { P, goto_515(obatsrd); }
-N(id_Maroon) { P, goto_515(obatsrd); }
-N(id_Lime) { P, goto_515(obatsrd); }
-N(id_Navy) { P, goto_515(obatsrd); }
-N(id_Blue) { P, goto_515(obatsrd); }
-N(id_Green) { P, goto_515(obatsrd); }
-N(id_Red) { P, goto_515(obatsrd); }
-N(id_Yellow) { P, goto_515(obatsrd); }
-
-N(id) {
-  P;
-  ο[σ - 5] = ρ_switch;
-  ο[σ - 4] = id_Olive;
-  ο[σ - 3] = id_Maroon;
-  ο[σ - 2] = id_Lime;
-  ο[σ - 1] = id_Navy;
-  ο[σ + 1] = id_Blue;
-  ο[σ + 2] = id_Green;
-  ο[σ + 3] = id_Red;
-  ο[σ + 4] = id_Yellow;
-  ο[σ + 5] = ρ_switch;
-  σ -= 11, goto_a(obatsrd);
-}
-N(S) { P; }
-
 int main() {
   long ram[2048];
   long *ο = ram + 512, β = 0, α = 0, τ = 0, σ = 1536, ρ = 4, δ = 1;
 
   ο[α++] = begin;
-  //  ο[α++] = "baaa";
-  //  ο[α++] = put;
-  //  ο[α++] = bat;
-  //  ο[α++] = var;
+  //  ο[α++] = "baaa";  ο[α++] = put;
+  ο[α++] = bat;     ο[α++] = var;
+  ο[α++] = bat;     ο[α++] = var;
+  ο[α++] = bat;     ο[α++] = var;
 
-  ο[α++] = id;
-  //  ο[α++] = print;
+  ο[α++] = print;
   ο[α++] = end;
   goto_a(obatsrd);
 }
