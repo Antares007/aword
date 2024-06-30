0219dbe1a9589eb98a55b8cf61b877fbf1d08239
Archil Bolkvadze <a.bolkvadze@gmail.com>
2024-06-30 22:14:46 +0400

    sophisticated multitasking

--- aword3.c
+++ aword3.c
@@ -1,4 +1,3 @@
-// clang-format on
 // \A/ord.
 extern int printf(const char *__restrict __format, ...);
 struct sContext {
@@ -10,26 +9,58 @@ struct sContext {
   long ray;
   long direction;
 };
-void goTo (struct sContext *s);
-void halt (struct sContext *s) {}
-void put  (struct sContext *s) {  printf("%c", (char)s->ram[s->text_index + 5]), goTo(s); }
-void call (struct sContext *s) {  s->ram[--s->call_stack] = s->text_index,
-                                  s->text_index = s->text_index + s->ram[s->text_index+5] * 11,
-                                  goTo(s); }
-void ret  (struct sContext *s) {  s->text_index = s->ram[s->call_stack++], goTo(s); }
-void (*sopcodes[])(struct sContext *s) = { halt, put, call, ret };
-void goTo (struct sContext *s) { (sopcodes[s->ram[s->text_index += 11]])(s); }
-
+void goTo(struct sContext *s);
+void halt(struct sContext *s) {}
+void put(struct sContext *s) {
+  printf("%c", (char)s->ram[s->text_index + 5]), goTo(s);
+}
+void call(struct sContext *s) {
+  s->ram[--s->call_stack] = s->text_index,
+  s->text_index = s->text_index + s->ram[s->text_index + 5] * 11, goTo(s);
+}
+void ret(struct sContext *s) {
+  s->text_index = s->ram[s->call_stack++], goTo(s);
+}
+void *h[2];
+void push(struct sContext *s) {
+  void **pushed = (void *)&s->ram[s->ram[s->advance] = s->advance + 1];
+  pushed[0] = h;
+  pushed[1] = h[1];
+  ((void **)h[1])[0] = pushed;
+  h[1] = pushed;
+}
+struct sContext *unshift() {
+  void **unshed = h[0];
+  ((void **)unshed[0])[1] = unshed[1];
+  ((void **)unshed[1])[0] = unshed[0];
+  return (struct sContext *)(unshed - (long)unshed[-1]);
+}
+void fork(struct sContext *s) {
+  push(s);
+  struct sContext sn = *s;
+  goTo(&sn);
+}
+void (*sopcodes[])(struct sContext *s) = {halt, put, call, ret, fork};
+void goTo(struct sContext *s) {
+  push(s);
+  s = unshift();
+  (sopcodes[s->ram[s->text_index += 11]])(s);
+}
 int main() {
+  h[1] = h[0] = h;
   struct sContext *s = &(struct sContext){};
+  s->advance = 0;
   s->call_stack = 512;
   s->text_index = s->space = s->call_stack + 5;
-  s->ram[s->space] = 0,                           s->space += 11, // halt
-  s->ram[s->space] = 2, s->ram[s->space+5] = 3,   s->space += 11, // call +3
-  s->ram[s->space] = 1, s->ram[s->space+5] = 'a', s->space += 11, // put 'a'
-  s->ram[s->space] = 1, s->ram[s->space+5] = 'b', s->space += 11, // put 'b'
-  s->ram[s->space] = 0,                           s->space += 11, // halt
-  s->ram[s->space] = 1, s->ram[s->space+5] = 't', s->space += 11, // put 't'
-  s->ram[s->space] = 3,                           s->space += 11, // ret
+  // clang-format off
+  s->ram[s->space] = 0,                             s->space += 11, // halt
+  s->ram[s->space] = 4,                             s->space += 11, // fork
+  s->ram[s->space] = 4,                             s->space += 11, // fork
+  s->ram[s->space] = 2, s->ram[s->space + 5] = 3,   s->space += 11, // call +3
+  s->ram[s->space] = 1, s->ram[s->space + 5] = 'a', s->space += 11, // put 'a'
+  s->ram[s->space] = 1, s->ram[s->space + 5] = 'b', s->space += 11, // put 'b'
+  s->ram[s->space] = 0,                             s->space += 11, // halt
+  s->ram[s->space] = 1, s->ram[s->space + 5] = 't', s->space += 11, // put 't'
+  s->ram[s->space] = 3,                             s->space += 11, // ret
   goTo(s);
 }
