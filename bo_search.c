869099a0980246ae5627d1f7ecb5ecbbeba8b5e1
Archil Bolkvadze <a.bolkvadze@gmail.com>
2024-09-26 19:38:09 +0400

    clean

--- bo_search.c
+++ bo_search.c
@@ -3,11 +3,11 @@ extern int strcmp(const char *, const char *);
 S(match) { (strcmp(o[τ + 1], o[α - 1]) ? Gor : God)(OS); }
 
 Nar(sn);
-S(sn_n) { O(sn), O(And), go_n(OS); }
-S(sn_name) { O(sn_n), O(Or), match(OS); }
+S(sn_n) { O(sn), O(go_n), Go(OS); }
+S(sn_name) { O(sn_n), O(Or), O(match), Go(OS); }
 Book_of_(sn, [name] = sn_name, [tab] = sn_n, [halt] = Gor, );
 
 Nar(ss);
-S(ss_s) { O(ss), O(And), go_s(OS); }
-S(ss_name) { O(ss_s), O(Or), match(OS); }
+S(ss_s) { O(ss), O(go_s), Go(OS); }
+S(ss_name) { O(ss_s), O(Or), O(match), Go(OS); }
 Book_of_(ss, [name] = ss_name, [tab] = ss_s, [halt] = Gor, );
--- bo_twist.c
+++ bo_twist.c
@@ -1,27 +1,36 @@
 #include "sophis2.h"
-Nar(bo_grow);
 Nar(twist);
-S(twist_e) { O(twist), O(And), go_e(OS); }
-S(cr) { τ = ((τ >> Σ) << Σ), Go(OS); }
-S(twist_s) { O(twist), O(go_s), cr(OS); }
 Nar(ss);
 Nar(sn);
-S(nearch) { τ = o[β++], O(sn), cr(OS); }
+S(cr) { τ = ((τ >> Σ) << Σ), Go(OS); }
+S(nearch) { τ = o[β++], O(sn), O(cr), Go(OS); }
 S(search) {
   o[α++] = o[τ + 1];
   O(drop_α);
-  O(τ);
-  O(G1), O(G1), O(nearch), O(NotAndOr);
-  O(ss), cr(OS);
+                  O(τ);
+  O(G1),  O(G1),  O(nearch), O(NotAndOr);
+          O(ss),
+          O(cr),
+          Go(OS);
 }
 S(sta) { τ = o[β++], Go(OS); }
 S(twist_tword) {
-  O(twist), O(And);
-  O(go_e), O(And);
-  O(τ);
-  O(G1), O(G1), O(sta), O(NotAndOr);
-  O(twist), O(And), search(OS);
+          O(twist),             O(And);
+          O(go_e),              O(And);
+                      O(τ);
+  O(G1),  O(G1),      O(sta),   O(NotAndOr);
+          O(twist),             O(And);
+          O(search);
+  Go(OS);   
 }
-Book_of_(
-    twist, [name] = twist_e, [nl] = twist_s, [tab] = twist_e, [print] = twist_e,
-    [put] = twist_e, [tword] = twist_tword, [dot] = Gor, );
+S(twist_s ) { O(twist), O(go_s), O(cr), Go(OS); }
+S(twist_e ) { O(twist), O(go_e), Go(OS); }
+Book_of_(twist,
+    [name ] = twist_e,
+    [tab  ] = twist_e,
+    [print] = twist_e,
+    [put  ] = twist_e,
+    [nl   ] = twist_s,
+    [tword] = twist_tword,
+    [dot  ] = Gor,
+);
